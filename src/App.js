9838f812f1fd0d2c7c0da6bbd2c0b4a87b039209
Julio Filizzola <juliofilizzola@hotmail.com>
2021-06-08 14:32:07 -0300

    creat browser router e switch route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-
+import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound } from './pages';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <div>
+      Movie Card Library CRUD
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" render={ () => <MovieList /> } />
+          <Route path="/movies/new" render={ () => <NewMovie /> } />
+          <Route
+            exact
+            path="/movies/:id"
+            render={ (props) => <MovieDetails { ...props } /> }
+          />
+          <Route path="/movies/:id/" render={ (props) => <EditMovie { ...props } /> } />
+          <Route NoMatch render={ () => <NotFound /> } />
+        </Switch>
+      </BrowserRouter>
+    </div>
   );
 }
 
