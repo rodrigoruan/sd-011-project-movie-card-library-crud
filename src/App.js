6f0017326ca7a211774041af1465d2cc99d28268
Julio Cesar <julio.cesarcj1402@gmail.com>
2021-06-08 15:14:28 -0300

    requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import NewMovie from './pages/NewMovie';
+import MovieDetails from './pages/MovieDetails';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <div>
+      <h1>Movie Card Library CRUD</h1>
+      <Switch>
+        <Route exact path="/" component={ MovieList } />
+        <Route exact path="/movies/new" component={ NewMovie } />
+        <Route exact path="/movies/:id" component={ MovieDetails } />
+        <Route exact path="/movies/:id/edit" component={ EditMovie } />
+        <Route exact component={ NotFound } />
+      </Switch>
+    </div>
   );
 }
 
