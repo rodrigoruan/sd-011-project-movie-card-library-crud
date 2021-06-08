2f4b25d73c8c07e392611d69d44924792f390322
anaclarabck <ackyotoku@gmail.com>
2021-06-08 17:58:14 -0300

    [ADD] requisito 3: link MovieCard

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,19 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, subtitle, storyline, imagePath, rating } = movie;
+    const { id, title, imagePath, storyline } = movie;
     return (
       <div className="movie-card" data-testid="movie-card">
         <img alt="Movie Cover" className="movie-card-image" src={ imagePath } />
         <div className="movie-card-body">
           <h4 data-testid="movie-card-title" className="movie-card-title">{title}</h4>
-          <h5 className="movie-card-subtitle">{subtitle}</h5>
           <p className="movie-card-storyline">{storyline}</p>
         </div>
-        <p rating={ rating } />
+        <Link to={ `/movies/${id}` }>VER DETALHES</Link>
       </div>
     );
   }
@@ -21,6 +21,7 @@ class MovieCard extends React.Component {
 
 MovieCard.propTypes = {
   movie: PropTypes.shape({
+    id: PropTypes.number,
     title: PropTypes.string,
     subtitle: PropTypes.string,
     storyline: PropTypes.string,
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,19 +1,45 @@
 import React, { Component } from 'react';
 
-// import * as movieAPI from '../services/movieAPI';
-// import { Loading } from '../components';
+import PropTypes from 'prop-types';
+import * as movieAPI from '../services/movieAPI';
+import { Loading } from '../components';
 
 class MovieDetails extends Component {
-  render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
+  constructor() {
+    super();
 
-    // removido title da const abaixo para passar no linter
-    const { storyline, imagePath, genre, rating, subtitle } = {};
+    this.state = {
+      movie: '',
+    };
 
+    this.setState = this.setState.bind(this);
+  }
+
+  componentDidMount() {
+    const movie = this.props;
+    console.log(movie);
+    const { id } = movie.match.params;
+    this.showMovie(id);
+  }
+
+  async showMovie(currentId) {
+    this.setState(
+      async () => {
+        const movieToShow = await movieAPI.getMovie(currentId);
+        this.setState({
+          movie: movieToShow,
+        });
+      },
+    );
+  }
+
+  renderMovie() {
+    const { movie } = this.state;
+    const { title, storyline, imagePath, genre, rating, subtitle } = movie;
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={ `../${imagePath}` } />
+        <p>{ `Title: ${title}` }</p>
         <p>{ `Subtitle: ${subtitle}` }</p>
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
@@ -21,6 +47,21 @@ class MovieDetails extends Component {
       </div>
     );
   }
+
+  render() {
+    const { movie } = this.state;
+    return (
+      movie ? this.renderMovie() : <Loading />
+    );
+  }
 }
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.number,
+    }),
+  }).isRequired,
+};
 
 export default MovieDetails;
