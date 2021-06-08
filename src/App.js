9818a1af62af8ec69842f391d6773a87e9452629
VictorS <czpask@gmail.com>
2021-06-08 14:36:32 -0300

    Iniciando projeto CRUD

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import NewMovie from './pages/NewMovie';
+import MovieDetails from './pages/MovieDetails';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <BrowserRouter>
+      <div>Movie Card Library CRUD</div>
+      <Switch>
+        <Route exact path="/" render={() => <MovieList />} />
+        <Route path="/movies/new" render={() => <NewMovie />} />
+        <Route exact path="/movies/:id" render={(props) => <MovieDetails {...props} />} />
+        <Route path="/movies/:id/edit" render={() => <EditMovie />} />
+        <Route NoMatch render={ () => <NotFound /> } /> 
+      </Switch>
+    </BrowserRouter>
   );
 }
 
