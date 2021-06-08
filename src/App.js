004b9685942276c782587fde1b0e0143e0b0a35a
Renan <renan.vamo@gmail.com>
2021-06-08 15:17:08 -0300

    Adiciona as rotas, o BrowserRouter e o Switch no App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import { EditMovie, MovieDetails, NewMovie, MovieList, NotFound } from './pages';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <Switch>
+      <BrowserRouter>
+        <div>Movie Card Library CRUD</div>
+        <Route exact path="/" component={ MovieList } />
+        <Route path="/movies/:id" component={ MovieDetails } />
+        <Route path="/movies/:id/edit" component={ EditMovie } />
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route path="*" component={ NotFound } />
+      </BrowserRouter>
+    </Switch>
   );
 }
 
