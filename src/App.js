733b0eca4af25878bf7d5adb49ea860cf418c545
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-06-08 17:19:27 -0300

    done first requirement

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { Route, Switch } from 'react-router';
+import MovieDetails from './pages/MovieDetails';
+import MovieList from './pages/MovieList';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
+      <>
         <div>Movie Card Library CRUD</div>
-      </BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ MovieList } />
+          <Route path="/movies/new" component={ NewMovie } />
+          <Route exact path="/movies/:id" component={ MovieDetails } />
+          <Route path="/movies/:id/edit" component={ EditMovie } />
+          <Route path="" component={ NotFound } />
+        </Switch>
+      </>
     );
   }
 }
