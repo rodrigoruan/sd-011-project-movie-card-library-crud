c20ffeb747fbe95bf7829d0f752d3ab77dc9db5f
YanParoni <yan.almeida.paroni@gmail.com>
2021-06-08 16:29:09 -0300

    Requisito 4

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -4,12 +4,33 @@ import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
+  constructor() {
+    super();
+    this.state = {
+      storyline:'',
+      subtitle:'',
+      genre:'action',
+      rating: 0,
+      imagePath:'',
+      loading: true,
+    };
+
+  }
+
+  componentDidMount() {
+    const {id} = this.props.match.params;
+
+    movieAPI.getMovie(id)
+      .then(({subtitle,storyline,genre,rating,imagePath}) => {
+        this.setState({ subtitle,storyline,genre,rating,imagePath, loading: false });
+      })
+      .catch((err) => console.error(err));
+  } 
+
   render() {
     // Change the condition to check the state
     // if (true) return <Loading />;
-
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
-
+    const {subtitle,storyline,genre,rating,imagePath} = this.state
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={ `../${imagePath}` } />
