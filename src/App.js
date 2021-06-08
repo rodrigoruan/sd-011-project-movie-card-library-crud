e9679247d85cf1a1fef9067e687bd1e8b75bad0b
André Jaques Batista <andrejaques.b@gmail.com>
2021-06-08 15:11:19 -0300

    add require 01 - App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+
+import { MovieList, NewMovie, MovieDetails, EditMovie, NotFound } from './pages/index';
 
 function App() {
   return (
     <Router>
       <div>Movie Card Library CRUD</div>
+      <Switch>
+        <Route path="/" exact>
+          <MovieList />
+        </Route>
+        <Route path="/movies/new" exact>
+          <NewMovie />
+        </Route>
+        <Route path="/movies/:id" exact>
+          <MovieDetails />
+        </Route>
+        <Route path="/movies/:id/edit" exact>
+          <EditMovie />
+        </Route>
+        <Route>
+          <NotFound />
+        </Route>
+      </Switch>
     </Router>
   );
 }
