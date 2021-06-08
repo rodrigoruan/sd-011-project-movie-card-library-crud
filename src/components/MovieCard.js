be79cdf1ef35a30403b3fbe6a47380ef6127d90a
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2021-06-08 18:13:12 -0300

    Requisito 3 realizado

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom"
 
 class MovieCard extends React.Component {
   render() {
@@ -8,6 +9,7 @@ class MovieCard extends React.Component {
         <img src={movie.imagePath} alt={`Capa do filme ${movie.title}`} />
         <h3>{movie.title}</h3>
         <h4>{movie.storyline}</h4>
+        <Link to={`movies/${movie.id}`} >VER DETALHES</Link>
       </div>
     );
   }
