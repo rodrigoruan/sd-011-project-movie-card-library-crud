fcdf4da8a90db46e84fc6bcdf16b1e7e8f19451c
Julio Filizzola <juliofilizzola@hotmail.com>
2021-06-08 14:41:17 -0300

    importando proptypes no movieCard

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import Proptypes from 'prop-types';
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
