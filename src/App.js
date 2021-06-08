bd9507dcb21cdd5220ee81b46c384dbe6681de9e
Ghislaine <glenox79@gmail.com>
2021-06-08 14:26:30 -0300

    iniciando projeto

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
+/* import { BrowserRouter } from './react-router-dom'; */
 
-function App() {
-  return (
-    <div>Movie Card Library CRUD</div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>Movie Card Library CRUD</div>
+    );
+  }
 }
 
 export default App;
