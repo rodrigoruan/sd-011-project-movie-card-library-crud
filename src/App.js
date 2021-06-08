0c13f414c1c66d13d912ea44caa2fedd33b2af87
Matheus_Alexandre <alexandremhm@gmail.com>
2021-06-08 15:32:02 -0300

    Add requisito 3

--- src/App.js
+++ src/App.js
@@ -12,9 +12,9 @@ function App() {
   return (
     <BrowserRouter>
       <div>Movie Card Library CRUD</div>
-      <Route path="/" component={ MovieList } />
+      <Route exact path="/" component={ MovieList } />
       <Route path="/movies/new" component={ NewMovie } />
-      <Route path="/movies/:id" component={ MovieDetails } />
+      <Route exact path="/movies/:id" component={ MovieDetails } />
       <Route path="/movies/:id/edit" component={ EditMovie } />
       <Route component={ NotFound } />
     </BrowserRouter>
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <h3>{ movie.title }</h3>
+        <img src={ movie.imagePath } alt={ movie.title } />
+        <p>{ movie.storyline }</p>
+        <Link to={ `/movies/${movie.id}` }>VER DETALHES</Link>
       </div>
     );
   }
