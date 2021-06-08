28de184e50f37494851feb5748b94c8f47776609
Alexandre <naitwa.alexandre@gmail.com>
2021-06-08 19:04:06 -0300

    01

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import NewMovie from './pages/NewMovie';
+import MovieDetails from './pages/MovieDetails';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 import './App.css';
 
 function App() {
   return (
     <Router>
-      <div>Movie Card Library CRUD</div>
+      <Switch>
+        <Route path="/" exact render={ () => <MovieList /> } />
+        <Route path="/movies/new" render={ () => <NewMovie /> } />
+        <Route exact path="/movies/:id" render={ () => <MovieDetails /> } />
+        <Route path="/movies/:id/edit" render={ () => <EditMovie /> } />
+        <Route path="/404" render={ () => <NotFound /> } />
+        <Redirect from="*" to="/404" />
+      </Switch>
     </Router>
   );
 }
