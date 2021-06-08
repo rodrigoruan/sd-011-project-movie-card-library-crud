224bef677c0f0bd184a044f4b65dfa8203d23792
Tales Coelho <tales-coelho@live.com>
2021-06-08 09:00:34 -0300

    Adicionando o NewMovie e MovieDetails ao BroserRouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { BroserRouter, Route } from 'react-router-dom';
 import MoveList from './pages/MovieList';
+import NewMovie from './pages/NewMovie';
+import MovieDetails from './pages/MovieDetails';
 
 function App() {
   return (
     <div>
       <BroserRouter>
         <Route exact path="/" component={ <MoveList /> } />
+        <Route exact path="/movies/new" component={ <NewMovie /> } />
+        <Route exact path="/movies/:id" component={ <MovieDetails /> } />
       </BroserRouter>
     </div>
   );
