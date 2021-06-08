0d5b5205513652a42f50f156c8d310fac510a308
Jean Esteves <jean.esteves2001@gmail.com>
2021-06-08 18:45:26 -0300

    app lint resolvido

--- src/App.js
+++ src/App.js
@@ -11,14 +11,14 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={MovieList} />
-        <Route path="/movies/new" component={NewMovie} />
-        <Route path="/movies/:id/edit" component={EditMovie} />
+        <Route exact path="/" component={ MovieList } />
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route path="/movies/:id/edit" component={ EditMovie } />
         <Route
           path="/movies/:id"
-          render={(props) => <MovieDetails {...props} />}
+          render={ (props) => <MovieDetails { ...props } /> }
         />
-        <Route path="*" component={NotFound} />
+        <Route path="*" component={ NotFound } />
       </Switch>
     </Router>
   );
