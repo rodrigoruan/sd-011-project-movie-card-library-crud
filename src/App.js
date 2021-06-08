ede73f2c4f9811148c6aa7239d83076e64661aa6
Hamander <1hamander@gmail.com>
2021-06-08 16:57:40 -0300

    Requisito_2 Completo

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import { Switch, BrowserRouter, Route } from 'react-router-dom'
+import { Switch, BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
-import MovieList from "./pages/MovieList";
-import NewMovie from "./pages/NewMovie";
-import MovieDetails from "./pages/MovieDetails";
-import EditMovie from "./pages/EditMovie";
-import NotFound from "./pages/NotFound";
+import MovieList from './pages/MovieList';
+import NewMovie from './pages/NewMovie';
+import MovieDetails from './pages/MovieDetails';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" render={() => (<MovieList />)}/>
-        <Route path="/movies/new" render={() => (<NewMovie />)}/>
-        <Route exact path="/movies/:id" render={() => (<MovieDetails />)}/>
-        <Route path="/movies/:id/edit" render={() => (<EditMovie />)}/>
-        <Route exact path="*" render={() => (<NotFound />)}/>
+        <Route exact path="/" render={ () => (<MovieList />) } />
+        <Route path="/movies/new" render={ () => (<NewMovie />) } />
+        <Route exact path="/movies/:id" render={ () => (<MovieDetails />) } />
+        <Route path="/movies/:id/edit" render={ () => (<EditMovie />) } />
+        <Route path="*" render={ () => (<NotFound />) } />
       </Switch>
     </BrowserRouter>
   );
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const { title, subtitle, storyline, imagePath } = movie;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <h2>
+          { title }
+        </h2>
+        <h4>
+          { subtitle }
+        </h4>
+        <p>
+          { storyline }
+        </p>
+        <img
+          src={ imagePath }
+          alt="movie art"
+        />
       </div>
     );
   }
 }
 
 export default MovieCard;
+
+MovieCard.propTypes = {
+  movie: PropTypes.arrayOf(PropTypes.object).isRequired,
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string.isRequired,
+  storyline: PropTypes.string.isRequired,
+  imagePath: PropTypes.string.isRequired,
+}
