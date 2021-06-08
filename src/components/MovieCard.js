30d95f102360935af193820d7a93d465de811281
Vitor <vitorguima@gmail.com>
2021-06-08 15:25:32 -0300

    requisito 3 finalizado

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const { title, storyline, imagePath, id } = movie;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <span>{ title }</span>
+        <img src={ imagePath } alt={`movie-${ title }`}/>
+        <p>{ storyline }</p>
+        <Link to={`/movies/${id}`}>VER DETALHES</Link>
       </div>
     );
   }
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
+import { Loading } from '../components';
 import MovieCard from '../components/MovieCard';
-
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
@@ -9,17 +9,37 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      loading: true,
     };
+
+    this.renderMovieCards = this.renderMovieCards.bind(this);
+  }
+
+  async componentDidMount() {
+    const { getMovies } = movieAPI;
+    const data = await getMovies();
+    this.setState(
+      ({ loading: true }), 
+      () => this.setState(() => ({
+        movies: data,
+        loading: false,
+      }))
+      )
+  }
+
+  renderMovieCards() {
+    const { movies } = this.state;
+    return (
+      movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)
+    )
   }
 
   render() {
-    const { movies } = this.state;
-
-    // Render Loading here if the request is still happening
+    const { loading } = this.state;
 
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        {loading ? <Loading /> : this.renderMovieCards()}
       </div>
     );
   }
