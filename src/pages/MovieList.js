5a45fd71910319d97864d5d9b31264cd6b376093
Luciano Pimenta <l.a.p.pimenta.j@gmail.com>
2021-06-08 19:25:48 -0300

    Requisito 6 - Concluido

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import MovieCard from '../components/MovieCard';
 import Loading from '../components/Loading';
 
@@ -35,12 +36,16 @@ class MovieList extends Component {
 
   render() {
     const { loading, movies } = this.state;
-    const loadingElement = <Loading />; // Render Loading here if the request is still happening
+
+    if (loading) {
+      return <Loading />;
+    }
 
     return (
       <div data-testid="movie-list">
-        { loading ? loadingElement : movies.map((movie) => (
-          <MovieCard key={ movie.title } movie={ movie } />)) }
+        {movies.map((movie) => (
+          <MovieCard key={ movie.title } movie={ movie } />))}
+        <Link to="/movies/new"> ADICIONAR CARTÃO </Link>
       </div>
     );
   }
