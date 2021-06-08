87bedba90b7b44ab06275778f1cf12559bbba1bc
Jean Esteves <jean.esteves2001@gmail.com>
2021-06-08 18:46:00 -0300

    newmovie lint

--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -28,7 +28,7 @@ class NewMovie extends Component {
     }
     return (
       <div data-testid="new-movie">
-        <MovieForm onSubmit={this.handleSubmit} />
+        <MovieForm onSubmit={ this.handleSubmit } />
       </div>
     );
   }
