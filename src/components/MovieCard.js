b1d03a936273d7c7e6d8d7e3185c948bce97dd0c
Gabriel <gabrielfernandessilva.ba@gmail.com>
2021-06-08 14:39:01 -0300

    Erros de Lint

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const {
+      id, title, subtitle, genre, imagePath, rating, storyline,
+    } = movie;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <h1>{ title }</h1>
+        <h1>{ subtitle }</h1>
+        <img src={ imagePath } alt="poke" />
+        <h1>{ genre }</h1>
+        <h1>{ rating }</h1>
+        <h1>{ storyline }</h1>
+        <Link to={ `/movies/${id}` } params={ id }>VER DETALHES</Link>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    genre: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    imagePath: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+    storyline: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default MovieCard;
