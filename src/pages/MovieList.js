8a3c442dcdc04a130d05236fff69bcedfdd0fec7
israeltonholo <israeltonholo@hotmail.com>
2021-06-08 16:41:50 -0300

    adiciona requisito 2

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
-
+import Loading from '../components/Loading';
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
   constructor() {
     super();
-
     this.state = {
       movies: [],
+      loading: true,
     };
   }
 
-  render() {
-    const { movies } = this.state;
+  componentDidMount() {
+    movieAPI.getMovies()
+      .then((movies) => {
+        this.setState({ movies, loading: false });
+      });
+  }
 
+  render() {
+    const { movies, loading } = this.state;
     // Render Loading here if the request is still happening
 
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        {loading ? <Loading />
+          : movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
       </div>
     );
   }
