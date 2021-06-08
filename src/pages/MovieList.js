bf8433d08f99ad9d2380eb6281ffe339e8c5b486
Daniel <danielfdoficial@gmail.com>
2021-06-08 15:51:38 -0300

    Requisito 1 ao 3, mas lint ainda nao vai passar

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
 
 import * as movieAPI from '../services/movieAPI';
+import movies from '../services/movieData';
 
 class MovieList extends Component {
   constructor() {
@@ -9,17 +10,33 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      load: true,
     };
+    this.updateMovie = this.updateMovie.bind(this);
+  }
+
+  async componentDidMount() {
+    await movieAPI.getMovies();
+    this.updateMovie();
+  }
+
+  updateMovie() {
+    this.setState((state) => ({
+      movies: [...state.movies, movies],
+      load: false,
+    }));
   }
 
   render() {
-    const { movies } = this.state;
+    const { load } = this.state;
 
     // Render Loading here if the request is still happening
+    const loading = 'Carregando...';
+    const movs = movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />);
 
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        {load ? loading : movs}
       </div>
     );
   }
