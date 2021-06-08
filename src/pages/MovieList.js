ae85eab355744bc04e061c4856703edec9838219
Daniel Leon <daniel.radialista@gmail.com>
2021-06-08 18:43:00 -0300

    requisito 2, pronto

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,21 +1,33 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
+import Loading from '../components/Loading';
 
-// import * as movieAPI from '../services/movieAPI';
+import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
       movies: [],
+      loading: true,
     };
   }
 
+  componentDidMount() { // Req. 2, a requisição deve ser feita no momento em que o MovieList for montado no DOM (componentDidMount).
+    movieAPI.getMovies().then((data) => {
+      this.setState({
+        movies: data,
+        loading: false,
+      });
+    });
+  }
+
   render() {
-    const { movies } = this.state;
+    const { movies, loading } = this.state;
 
     // Render Loading here if the request is still happening
+    if (loading) return <Loading />; // Req. 2, enquanto a requisição estiver em curso, MovieList deve renderizar o componente Loading.
 
     return (
       <div data-testid="movie-list">
