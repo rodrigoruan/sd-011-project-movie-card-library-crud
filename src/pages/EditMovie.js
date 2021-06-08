4c6f5b0bbfc624970c52e4ee46a353e7f945f4c8
Matheus_Alexandre <alexandremhm@gmail.com>
2021-06-08 16:28:42 -0300

    Add requisito 5

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,28 +1,53 @@
 import React, { Component } from 'react';
-
-import { MovieForm } from '../components';
+import { Redirect } from 'react-router';
+import { MovieForm, Loading } from '../components';
 import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      status: 'loading',
+      shouldRedirect: false,
+      movie: {},
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  componentDidMount() {
+    this.fetchApi();
+  }
+
   handleSubmit(updatedMovie) {
+    const { updateMovie } = movieAPI;
+    updateMovie(updatedMovie);
+    this.setState({
+      shouldRedirect: true,
+    });
+  }
+
+  async fetchApi() {
+    const { id } = this.props.match.params;
+    const { getMovie } = movieAPI;
+    this.setState({ status: 'loading' },
+      async () => {
+        const requestResponse = await getMovie(id);
+        this.setState({
+          status: '',
+          movie: requestResponse,
+        });
+      });
   }
 
   render() {
     const { status, shouldRedirect, movie } = this.state;
     if (shouldRedirect) {
-      // Redirect
+      return <Redirect to="/" />;
     }
 
     if (status === 'loading') {
-      // render Loading
+      return <Loading />;
     }
-
     return (
       <div data-testid="edit-movie">
         <MovieForm movie={ movie } onSubmit={ this.handleSubmit } />
