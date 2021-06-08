6188afb5df62ed6fd5d1cf6bae22d53ac51b35f4
Rodrigo Ruan <rodrigopython16@gmail.com>
2021-06-08 15:37:33 -0300

    Adiciona requisito 5

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,26 +1,47 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router';
 
-import { MovieForm } from '../components';
+import { Loading, MovieForm } from '../components';
 import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+
+    this.state = {
+      movie: '',
+      status: 'loading',
+      shouldRedirect: false,
+    };
+
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.fetchMovie = this.fetchMovie.bind(this);
+  }
+
+  componentDidMount() {
+    const { pathname } = window.location;
+    const id = pathname.match(/(?<=\/)\d+(?=\/)/);
+    this.fetchMovie(id);
   }
 
   handleSubmit(updatedMovie) {
+    movieAPI.updateMovie(updatedMovie);
+    this.setState({ shouldRedirect: true });
+  }
+
+  async fetchMovie(id) {
+    const response = await movieAPI.getMovie(id);
+    this.setState({ movie: response, status: '' });
   }
 
   render() {
     const { status, shouldRedirect, movie } = this.state;
     if (shouldRedirect) {
-      // Redirect
+      return <Redirect to="/" />;
     }
 
     if (status === 'loading') {
-      // render Loading
+      return <Loading />;
     }
 
     return (
