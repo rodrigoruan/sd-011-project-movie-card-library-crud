30344f877937213229ffd694666cd513424128c0
Jean Esteves <jean.esteves2001@gmail.com>
2021-06-08 19:20:59 -0300

    moviecard e movieform proptypes e lint

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
@@ -8,10 +9,18 @@ class MovieCard extends React.Component {
       <div data-testid="movie-card">
         <h4>{movie.title}</h4>
         <p>{movie.storyline}</p>
-        <Link to={`/movies/${movie.id}`}>VER DETALHES</Link>
+        <Link to={ `/movies/${movie.id}` }>VER DETALHES</Link>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    storyline: PropTypes.string,
+    id: PropTypes.number,
+  }).isRequired,
+};
 
 export default MovieCard;
