a9fcaf4ffa1c9311747a292c2cf28fe65eb8df4b
Diego Ribeiro <dihrm2001@gmail.com>
2021-06-08 15:19:13 -0300

    Requisito 2

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -7,19 +7,35 @@ class MovieList extends Component {
   constructor() {
     super();
 
+    this.fetchAPI = this.fetchAPI.bind(this);
+
     this.state = {
+      loading: true,
       movies: [],
     };
   }
 
+  componentDidMount() {
+    this.fetchAPI();
+  }
+
+  fetchAPI() {
+    this.setState(() => ({ loading: true }), async () => {
+      const movies = await movieAPI.getMovies();
+      this.setState({ movies, loading: false });
+    });
+  }
+
   render() {
-    const { movies } = this.state;
-
-    // Render Loading here if the request is still happening
-
+    const { movies, loading } = this.state;
+    const loadingElement = <p>Carregando...</p>;
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        {
+          loading
+            ? loadingElement
+            : movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)
+        }
       </div>
     );
   }
