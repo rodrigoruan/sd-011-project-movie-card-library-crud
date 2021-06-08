b6f968709e19f9b813706a016f990556f6343810
nandovbr <nandovbr15@hotmail.com>
2021-06-08 15:25:01 -0300

    requisito 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
+import { EditMovie, MovieDetails, MovieList, NewMovie, NotFound } from './pages';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <div>Movie Card Library CRUD</div>
-    </Router>
+      <Switch>
+        <Route exact path="/" component={ MovieList } />
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route exact path="/movies/:id" component={ MovieDetails } />
+        <Route exact path="/movies/:id/edit" component={ EditMovie } />
+        <Route exact path="" component={ NotFound } />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -11,3 +11,36 @@ class MovieCard extends React.Component {
 }
 
 export default MovieCard;
+
+// import React from 'react';
+// import PropTypes from 'prop-types';
+
+// class MovieCard extends React.Component {
+//   render() {
+//     const { movie } = this.props;
+//     const { title, subtitle, storyline, rating, imagePath } = movie;
+//     return (
+//       <div className="movie-card" data-testid="movie-card">
+//         <img alt="Movie Cover" className="movie-card-image" src={ imagePath } />
+//         <div className="movie-card-body">
+//           <h4 data-testid="movie-card-title" className="movie-card-title">{title}</h4>
+//           <h5 className="movie-card-subtitle">{subtitle}</h5>
+//           <p className="movie-card-storyline">{storyline}</p>
+//         </div>
+//         <p>{rating}</p>
+//       </div>
+//     );
+//   }
+// }
+
+// MovieCard.propTypes = {
+//   movie: PropTypes.shape({
+//     title: PropTypes.string,
+//     subtitle: PropTypes.string,
+//     storyline: PropTypes.string,
+//     rating: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+//     imagePath: PropTypes.string,
+//   }).isRequired,
+// };
+
+// export default MovieCard;
