b19a33a4c20afb7bb03af30518de58bb8399530a
Vitor <vitorguima@gmail.com>
2021-06-08 14:53:37 -0300

    req 1 finalizado

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import NewMovie from './pages/NewMovie';
+import MovieDetails from './pages/MovieDetails';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
+    <>
     <div>Movie Card Library CRUD</div>
+    <BrowserRouter>
+    <Switch>
+      <Route path="/movies/:id/edit" render={ (props) => <EditMovie { ...props }/> }/>
+      <Route exact path="/movies/new" component={ NewMovie }/>
+      <Route path="/movies/:id" render={ (props) => <MovieDetails { ...props }/>} />
+      <Route path="/notfound" component={ NotFound } />
+      <Route exact path="/" component={ MovieList } />
+      <Route render={() => <Redirect to={{pathname: "/notfound"}} />} />
+    </Switch>
+    </BrowserRouter>
+    </>
   );
 }
 
