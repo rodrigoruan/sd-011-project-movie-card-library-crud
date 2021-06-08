94264d77d51835fab8fddbb44f712f592af26e50
fernando lasmar <f3rnandolasmar@gmail.com>
2021-06-08 19:22:46 -0300

    requisito 1 Ufa

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import NewMovie from './pages/NewMovie';
+import MovieDetails from './pages/MovieDetails';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
+import MovieList from './pages/MovieList';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div>Movie Card Library CRUD</div>
-    </Router>
+    <main>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" render={ () => <MovieList /> } />
+          <Route path="/movies/new" render={ () => <NewMovie /> } />
+          <Route exact path="/movies/:id" render={ () => <MovieDetails /> } />
+          <Route path="/movies/:id/edit" render={ () => <EditMovie /> } />
+          <Route path="/*" render={ () => <NotFound /> } />
+        </Switch>
+      </BrowserRouter>
+    </main>
   );
 }
 
