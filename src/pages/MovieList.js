849b3db3c24ee47defe21664e85da113387f9ee4
Luciano Pimenta <l.a.p.pimenta.j@gmail.com>
2021-06-08 15:22:31 -0300

    Requisitos 1 e 2 - Concluidos

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
+import Loading from '../components/Loading';
 
 import * as movieAPI from '../services/movieAPI';
 
@@ -8,18 +9,42 @@ class MovieList extends Component {
     super();
 
     this.state = {
+      loading: true,
       movies: [],
     };
+
+    this.renderCards = this.renderCards.bind(this);
+    this.fetchMovie = this.fetchMovie.bind(this);
+  }
+
+  async fetchMovie() {
+    this.setState(
+      { loading: true },
+      async () => {
+      const requestObject = await movieAPI.getMovies();
+      this.setState({ 
+        loading: false,
+        movies: requestObject,
+      })
+    })
+  }
+
+  componentDidMount() {
+    this.fetchMovie();
+  }
+
+  renderCards() {
+    const { movies } = this.state;
+    {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
   }
 
   render() {
-    const { movies } = this.state;
-
-    // Render Loading here if the request is still happening
+    const { loading } = this.state;
+    const loadingElement = <Loading /> // Render Loading here if the request is still happening
 
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        {loading ? loadingElement : this.renderCards() }
       </div>
     );
   }
