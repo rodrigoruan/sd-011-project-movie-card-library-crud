16f6074212ec0ba27cc2817fef399814f0d89a72
Matheus_Alexandre <alexandremhm@gmail.com>
2021-06-08 16:00:08 -0300

    Add requisito 4

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route} from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import MovieList from './pages/MovieList';
 import NewMovie from './pages/NewMovie';
 import MovieDetails from './pages/MovieDetails';
@@ -14,7 +14,7 @@ function App() {
       <div>Movie Card Library CRUD</div>
       <Route exact path="/" component={ MovieList } />
       <Route path="/movies/new" component={ NewMovie } />
-      <Route exact path="/movies/:id" component={ MovieDetails } />
+      <Route exact path="/movies/:id" render={ (props) => <MovieDetails { ...props } /> } />
       <Route path="/movies/:id/edit" component={ EditMovie } />
       <Route component={ NotFound } />
     </BrowserRouter>
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,22 +1,51 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
-  render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
+  constructor() {
+    super();
+    this.state = {
+      loading: false,
+      movie: {},
+    };
+    this.fetchApi = this.fetchApi.bind(this);
+  }
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+  componentDidMount() {
+    this.fetchApi();
+  }
+
+  async fetchApi() {
+    const { id } = this.props.match.params;
+    const { getMovie } = movieAPI;
+    this.setState({ loading: true },
+      async () => {
+        const requestResponse = await getMovie(id);
+        this.setState({
+          loading: false,
+          movie: requestResponse,
+        });
+      });
+  }
+
+  render() {
+    const { loading } = this.state;
+    if (loading === true) return <Loading />;
+    const { movie } = this.state;
+    const { title, storyline, imagePath, genre, rating, subtitle, id } = movie;
 
     return (
       <div data-testid="movie-details">
+        <p>{ `Title: ${title}` }</p>
         <img alt="Movie Cover" src={ `../${imagePath}` } />
         <p>{ `Subtitle: ${subtitle}` }</p>
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
+        <Link to="/">VOLTAR</Link>
+        <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
       </div>
     );
   }
