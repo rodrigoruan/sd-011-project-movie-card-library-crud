ab78bc088da9fba1a3146187d51ad82ab235af64
Jessica Arruda <sicarruda@gmail.com>
2021-06-08 17:37:55 -0300

    exercicio 2

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import MovieCard from '../components/MovieCard';
-
-import * as movieAPI from '../services/movieAPI';
+import React, { Component } from "react";
+import Loading from "../components/Loading";
+import MovieCard from "../components/MovieCard";
+import * as movieAPI from "../services/movieAPI";
 
 class MovieList extends Component {
   constructor() {
@@ -11,21 +11,24 @@ class MovieList extends Component {
       movies: [],
     };
   }
+  async componentDidMount() {
+    const response = await movieAPI.getMovies();
+    this.setState({ movies: response });
+  }
 
   render() {
     const { movies } = this.state;
 
     // Render Loading here if the request is still happening
 
-    return (
+    return movies.length >0? (
       <div data-testid="movie-list">
-
         {movies.map((movie) => (
-          <MovieCard key={ movie.title } movie={ movie } />
+          <MovieCard key={movie.title} movie={movie} />
+          
         ))}
-
       </div>
-    );
+    ):(<Loading/>);
   }
 }
 
