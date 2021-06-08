94005014785459cda634eecd9d0f67649215e8eb
Luciano Pimenta <l.a.p.pimenta.j@gmail.com>
2021-06-08 18:58:46 -0300

    Atualizacoes lint

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Redirect } from 'react-router-dom';
-import { Loading } from '../components';
-
-import { MovieForm } from '../components';
+import { Loading, MovieForm } from '../components';
 import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
