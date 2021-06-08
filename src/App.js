dd4c96a178eeddca9799acf4dd69370de8462538
Víctor Faria <vitilevi@hotmail.com>
2021-06-08 15:12:33 -0300

    Feito req 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import EditMovie from './pages/EditMovie';
+import MovieDetails from './pages/MovieDetails';
+import MovieList from './pages/MovieList';
+import NewMovie from './pages/NewMovie';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <div>Movie Card Library CRUD</div>
-    </Router>
+      <Switch>
+        <Route exact path="/">
+          <MovieList />
+        </Route>
+        <Route path="/movies/new">
+          <NewMovie />
+        </Route>
+        <Route path="/movies/:id">
+          <MovieDetails />
+          <Route
+            path="/movies/:id/edit"
+            render={ (props) => (<EditMovie { ...props } />) }
+          />
+        </Route>
+        <Route path="*">
+          <NotFound />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
