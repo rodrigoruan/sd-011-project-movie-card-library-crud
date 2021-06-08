c6d03bee2b52209ba5d4ee2e3e766ee1cafa883a
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-06-08 17:53:54 -0300

    Adiciona requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import NewMovie from './pages/NewMovie';
+import MovieDetails from './pages/MovieDetails';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/movies/:id/edit" render={ () => <EditMovie /> } />
+        <Route path="/movies/new" render={ () => <NewMovie /> } />
+        <Route path="/movies/:id" render={ () => <MovieDetails /> } />
+        <Route exact path="/" render={ () => <MovieList /> } />
+        <Route path="*" render={ () => <NotFound /> } />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
