551b998f1a30f301eb59f04e63ad309778358400
Arthur Hermann <arthurhermannc@gmail.com>
2021-06-08 17:21:46 -0300

    requisito 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import MovieList from './pages/MovieList';
 import NewMovie from './pages/NewMovie';
 import MovieDetails from './pages/MovieDetails';
@@ -9,17 +9,15 @@ import NotFound from './pages/NotFound';
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Movie Card Library CRUD</h1>
-        <BrowserRouter>
+      <BrowserRouter>
+        <Switch>
           <Route exact path="/" component={ MovieList } />
-          <Route path="/movies/new" component={ NewMovie } />
-          <Route path="/movies/:id" component={ MovieDetails } />
-          <Route path="/movies/:id/edit" component={ EditMovie } />
-          <Route path="" component={ NotFound } />
-        </BrowserRouter>
-      </div>
-
+          <Route exact path="/movies/new" component={ NewMovie } />
+          <Route exact path="/movies/:id" component={ MovieDetails } />
+          <Route exact path="/movies/:id/edit" component={ EditMovie } />
+          <Route path="*" component={ NotFound } />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
