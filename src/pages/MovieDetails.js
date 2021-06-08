e1fd6fece4bd019b96213676c82363476e8b06d0
Julio Filizzola <juliofilizzola@hotmail.com>
2021-06-08 15:03:26 -0300

    Edit MovieDetails

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -11,12 +11,18 @@ class MovieDetails extends Component {
     const { title, storyline, imagePath, genre, rating, subtitle } = {};
 
     return (
-      <div data-testid="movie-details">
-        <img alt="Movie Cover" src={ `../${imagePath}` } />
-        <p>{ `Subtitle: ${subtitle}` }</p>
-        <p>{ `Storyline: ${storyline}` }</p>
-        <p>{ `Genre: ${genre}` }</p>
-        <p>{ `Rating: ${rating}` }</p>
+      <div>
+        {loading ? (
+          <Loading />
+        ) : (
+          <div data-testid="movie-details">
+            <img alt="Movie Cover" src={ `../${imagePath}` } />
+            <p>{ `Title: ${title}` }</p>
+            <p>{ `Subtitle: ${subtitle}` }</p>
+            <p>{ `Storyline: ${storyline}` }</p>
+            <p>{ `Genre: ${genre}` }</p>
+            <p>{ `Rating: ${rating}` }</p>
+          </div>)}
       </div>
     );
   }
