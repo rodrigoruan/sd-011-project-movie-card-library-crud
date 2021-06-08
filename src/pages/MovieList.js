a5400547c501b2a0758da61a4fc9f15fcc3f2e30
Víctor Faria <vitilevi@hotmail.com>
2021-06-08 16:00:01 -0300

    Feito req 2

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
+import { Loading } from '../components';
 import MovieCard from '../components/MovieCard';
-
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
@@ -9,17 +9,39 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      loading: false,
     };
+    this.updateState = this.updateState.bind(this);
+  }
+
+  componentDidMount() {
+    this.updateState();
+  }
+
+  updateState() {
+    this.setState(() => ({
+      loading: true,
+    }),
+    async () => {
+      const info = await movieAPI.getMovies();
+      this.setState({
+        movies: info,
+        loading: false,
+      });
+    });
   }
 
   render() {
-    const { movies } = this.state;
+    const { movies, loading } = this.state;
 
     // Render Loading here if the request is still happening
-
+    const movieCard = movies.map(
+      (movie) => <MovieCard key={ movie.title } movie={ movie } />,
+    );
+    const load = loading ? <Loading /> : movieCard;
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        { load }
       </div>
     );
   }
