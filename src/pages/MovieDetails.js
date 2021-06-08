7f412894bf27067eec9e6fdc099c693d5f93c51a
Gisele Costa <jikinha@gmail.com>
2021-06-08 16:06:40 -0300

    lint

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import * as movieAPI from '../services/movieAPI';
-import { Loading } from '../components';
+import Loading from '../components/Loading';
 
 class MovieDetails extends Component {
   constructor() {
