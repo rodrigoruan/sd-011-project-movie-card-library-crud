9613361a039f3dc26572e939870b1e1eb99435f6
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-06-08 18:27:51 -0300

    requisito 7 implementado

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -10,20 +10,26 @@ class MovieDetails extends Component {
     this.state = {
       loading: true,
     };
-    this.fetchmovie = this.fetchmovie.bind(this);
+    this.fetchMovie = this.fetchMovie.bind(this);
+    this.deleteMovie = this.deleteMovie.bind(this);
   }
 
   componentDidMount() {
-    this.fetchmovie();
+    this.fetchMovie();
   }
 
-  async fetchmovie() {
+  async fetchMovie() {
     const { match: { params: { id } } } = this.props;
     const movie = await movieAPI.getMovie(id);
     this.setState({
       movie,
       loading: false,
     });
+  }
+
+  async deleteMovie() {
+    const { movie: { id } } = this.state;
+    movieAPI.deleteMovie(id);
   }
 
   render() {
@@ -41,6 +47,7 @@ class MovieDetails extends Component {
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
         <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
+        <Link to="/" onClick={ this.deleteMovie }>DELETAR</Link>
         <Link to="/">VOLTAR</Link>
       </div>
     );
