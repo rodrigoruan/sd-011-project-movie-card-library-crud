46c6988da9882892696d68c3881d44c26498b203
Alberto Candido <albertocandido98@gmail.com>
2021-06-08 16:48:42 -0300

    Finaliza requisito 6

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
-import { Link } from 'react-router-dom';
 
 class MovieDetails extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      loading:true,
+      loading: true,
       movie: [],
     };
   }
@@ -18,7 +17,8 @@ class MovieDetails extends Component {
   }
 
   async fechApi() {
-    const { id } = this.props.match.params
+    const { match } = this.props;
+    const { id } = match.params;
     const movieResolve = await movieAPI.getMovie(id);
     this.setState({
       movie: movieResolve,
@@ -29,7 +29,7 @@ class MovieDetails extends Component {
   render() {
     // Change the condition to check the state
     // if (true) return <Loading />;
-    const { movie, loading } = this.state 
+    const { movie, loading } = this.state;
     const { title, storyline, imagePath, genre, rating, subtitle, id } = movie;
 
     return loading ? <Loading /> : (
@@ -41,7 +41,7 @@ class MovieDetails extends Component {
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
         <Link to="/">VOLTAR</Link>
-        <Link to={`/movies/${id}/edit`}>EDITAR</Link>
+        <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
       </div>
     );
   }
