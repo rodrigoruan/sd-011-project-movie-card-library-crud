5677ac06add8a71b77be044347aa389a4b5af272
Daniel Leon <daniel.radialista@gmail.com>
2021-06-09 16:53:35 -0300

    requisito 4, pronto

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,26 +1,59 @@
 import React, { Component } from 'react';
-
-// import * as movieAPI from '../services/movieAPI';
-// import { Loading } from '../components';
+import PropTypes from 'prop-types';
+import { Link, Redirect } from 'react-router-dom';
+import * as movieAPI from '../services/movieAPI';
+import { Loading } from '../components';
 
 class MovieDetails extends Component {
-  render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
+  constructor(props) {
+    super(props);
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
-    console.log(title);
+    this.state = {
+      movie: [],
+      loading: true,
+      shouldRedirect: false,
+    };
+  }
+
+  componentDidMount() {
+    const { match: { params: { id } } } = this.props;
+    movieAPI.getMovie(id).then((data) => { // Req.4, chamando a função getMovie
+      this.setState({
+        movie: data,
+        loading: false,
+      });
+    });
+  }
+
+  render() {
+    const { movie: { title, storyline,
+      imagePath, genre, rating, subtitle, id }, loading, shouldRedirect } = this.state; // desestruturando os estados
+
+    // Change the condition to check the state
+    if (loading) return <Loading />;
+    if (shouldRedirect) return <Redirect to="/" />; // o shouldRedirect poderia ter qualquer nome, exemplo: "xablau"
 
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={ `../${imagePath}` } />
-        <p>{ `Subtitle: ${subtitle}` }</p>
-        <p>{ `Storyline: ${storyline}` }</p>
-        <p>{ `Genre: ${genre}` }</p>
-        <p>{ `Rating: ${rating}` }</p>
+        <p>{`Title: ${title}`}</p>
+        <p>{`Subtitle: ${subtitle}`}</p>
+        <p>{`Storyline: ${storyline}`}</p>
+        <p>{`Genre: ${genre}`}</p>
+        <p>{`Rating: ${rating}`}</p>
+        <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
+        <Link to="/">VOLTAR</Link>
       </div>
     );
   }
 }
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }).isRequired,
+};
 
 export default MovieDetails;
