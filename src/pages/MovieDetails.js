856ca4d5146aec4ca3fcb32c48357d4385c3a675
Gustavo Lemes <gustavolemes997@gmail.com>
2021-06-09 15:11:35 -0300

    Requisito 4

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,22 +1,63 @@
 import React, { Component } from 'react';
-
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
+import { Link } from 'react-router-dom';
 
 class MovieDetails extends Component {
-  render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
+  constructor() {
+    super();
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    this.state = {
+      id: 0,
+      movie: {},
+      load: true,
+    };
+
+    this.getMovieData = this.getMovieData.bind(this);
+    this.renderMovie = this.renderMovie.bind(this);
+  }
+
+  async getMovieData(movieId) {
+    const { id } = this.props.match.params;
+    const movie = await movieAPI.getMovie(id);
+    this.setState({ 
+      id: id,
+      movie: movie,
+      load: false
+    });
+  }
+  
+  renderMovie() {
+    const { title, storyline, imagePath, genre, rating, subtitle } = this.state.movie;
 
     return (
       <div data-testid="movie-details">
+        <h1>{ title }</h1>
         <img alt="Movie Cover" src={ `../${imagePath}` } />
         <p>{ `Subtitle: ${subtitle}` }</p>
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    this.getMovieData();
+  }
+
+  render() {
+
+    const { load, id } = this.state;
+    const loadingComponent = <Loading />;
+    const movieComponent = this.renderMovie();
+    const editPath = `/movies/${id}/edit`;
+
+    return (
+      <div>
+        { load ? loadingComponent : movieComponent }
+        <Link to="/" >VOLTAR</Link>
+        <Link to={ editPath } >EDITAR</Link>
       </div>
     );
   }
