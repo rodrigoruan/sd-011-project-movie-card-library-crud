874bb4c6e66d1bf33a9339b6a4a8342b8d406589
fernando lasmar <f3rnandolasmar@gmail.com>
2021-06-09 17:41:57 -0300

    requisito 3

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie: { title, storyline, id } } = this.props;
     return (
       <div data-testid="movie-card">
         Movie Card
+        <nav>
+          <h2>{title}</h2>
+          <p>{storyline}</p>
+          <Link to={ `movies/${id}` }>VER DETALHES</Link>
+        </nav>
       </div>
     );
   }
