c84a399c8288f363229023e22b6a5d2e569a0331
Gustavo Mendes <gusm217@gmail.com>
2021-06-09 16:23:11 -0300

    Adiciona o 2

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,9 +2,15 @@ import React from 'react';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie: { title, storyline, imagePath, genre, rating, subtitle } } = this.props;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <h3>{title}</h3>
+        <h4>{subtitle}</h4>
+        <img src={ imagePath } alt="imagens dos filmes" />
+        <p>{storyline}</p>
+        <p>{genre}</p>
+        <span>{rating}</span>
       </div>
     );
   }
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Loading } from '../components';
+// import PropTypes from 'prop-types';
 import MovieCard from '../components/MovieCard';
 
 import * as movieAPI from '../services/movieAPI';
@@ -10,19 +12,35 @@ class MovieList extends Component {
     this.state = {
       movies: [],
     };
+
+    this.gettingMovies = this.gettingMovies.bind(this);
+  }
+
+  componentDidMount() {
+    this.gettingMovies();
+  }
+
+  async gettingMovies() {
+    const response = await movieAPI.getMovies();
+
+    this.setState({
+      movies: response,
+    });
   }
 
   render() {
     const { movies } = this.state;
-
-    // Render Loading here if the request is still happening
-
     return (
       <div data-testid="movie-list">
+        <Loading />
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
       </div>
     );
   }
 }
+
+// MovieList.propTypes = {
+//   getMovies: PropTypes.func.isRequired,
+// };
 
 export default MovieList;
