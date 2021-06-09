45543a89ce297c7ef2e6d8a7742d31c35dfea11f
LeoFuna <leo.funa@gmail.com>
2021-06-09 07:52:27 -0300

    Requisito 6

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import MovieCard from '../components/MovieCard';
 import Loading from '../components/Loading';
 
@@ -33,6 +34,7 @@ class MovieList extends Component {
         {movies
           ? movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)
           : <Loading />}
+        <Link to="/movies/new">ADICIONAR CARTÃO</Link>
       </div>
     );
   }
