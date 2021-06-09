eca841a19dadb274772a6e76a5f62edf0cef4a41
Laura Gusmao <laurargus@gmail.com>
2021-06-09 18:15:19 -0300

    Adds link to NewMovie in main page and, in NewMovie, enables user to add a new movie by completing a form

--- src/App.js
+++ src/App.js
@@ -6,22 +6,20 @@ import { MovieList, NewMovie, MovieDetails, EditMovie, NotFound } from './pages'
 function App() {
   return (
     <BrowserRouter>
-      <main>
-        <div>Movie Card Library CRUD</div>
-        <Switch>
-          <Route exact path="/" component={ MovieList } />
-          <Route exact path="/movies/new" component={ NewMovie } />
-          <Route
-            path="/movies/:id/edit"
-            render={ (props) => <EditMovie { ...props } /> }
-          />
-          <Route
-            path="/movies/:id"
-            render={ (props) => <MovieDetails { ...props } /> }
-          />
-          <Route path="*" component={ NotFound } />
-        </Switch>
-      </main>
+      <div>Movie Card Library CRUD</div>
+      <Switch>
+        <Route exact path="/" component={ MovieList } />
+        <Route exact path="/movies/new" component={ NewMovie } />
+        <Route
+          path="/movies/:id/edit"
+          render={ (props) => <EditMovie { ...props } /> }
+        />
+        <Route
+          path="/movies/:id"
+          render={ (props) => <MovieDetails { ...props } /> }
+        />
+        <Route path="*" component={ NotFound } />
+      </Switch>
     </BrowserRouter>
   );
 }
