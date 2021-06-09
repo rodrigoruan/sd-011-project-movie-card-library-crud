6dc533e5be9feb33fe3352d088eb28f6adb1ba47
OldRoge <rogeriomrjr@icloud.com>
2021-06-09 15:59:16 -0300

    Done requirement 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter, BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+
+import MovieList from './pages/MovieList';
+import NewMovie from './pages/NewMovie';
+import MovieDetails from './pages/MovieDetails';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <Router>
-      <div>Movie Card Library CRUD</div>
-    </Router>
+    <BrowserRouter>
+      <Router>
+        <div>Movie Card Library CRUD</div>
+        <Route exact path="/movies/new" component={ NewMovie } />
+        <Route path="/movies/:id" render={ (props) => <MovieDetails { ...props } /> } />
+        <Route path="/movies/:id/edit" render={ () => <EditMovie /> } />
+        <Route exact path="/" component={ MovieList } />
+        <Route component={ NotFound } />
+      </Router>
+    </BrowserRouter>
   );
 }
 
