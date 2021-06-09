d0dff76c739af7dc5e07cc01b3a11f69be92dacc
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-06-08 21:50:57 -0300

    Requisito 7

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -44,6 +44,7 @@ class MovieDetails extends Component {
           <p>{ `Genre: ${genre}` }</p>
           <p>{ `Rating: ${rating}` }</p>
         </div>
+        <Link to="/" onClick={ () => movieAPI.deleteMovie(id) }>DELETAR</Link>
         <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
         <Link to="/">VOLTAR</Link>
       </div>
