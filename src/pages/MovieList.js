862433f9096ae2868031d5783cd48d92038d671a
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2021-06-09 17:53:26 -0300

    Req 2

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
-
+import Loading from '../components/Loading';
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
@@ -9,18 +9,31 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      loading: true,
     };
   }
 
+  async fetchMovies() {
+    const requestMovies = await movieAPI.getMovies()
+    this.setState({
+      movies: requestMovies,
+      loading: false,
+    })
+  }
+
+  componentDidMount() {
+    this.fetchMovies()
+  }
+
   render() {
-    const { movies } = this.state;
+    const { movies, loading } = this.state;
 
+    const request = loading ? <Loading /> : <div data-testid="movie-list">
+    {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+  </div>
     // Render Loading here if the request is still happening
-
     return (
-      <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
-      </div>
+      { request }
     );
   }
 }
