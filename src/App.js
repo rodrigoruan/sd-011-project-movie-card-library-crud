7bf08d1816c391da46ab23bbce2abd654f103818
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-06-09 18:22:10 -0300

    Altera ordem das rotas

--- src/App.js
+++ src/App.js
@@ -13,9 +13,9 @@ function App() {
       <div>Movie Card Library CRUD</div>
       <Switch>
         <Route path="/movies/new" component={ NewMovie } />
-        <Route path="/movies/:id" component={ MovieDetails } />
         <Route path="/movies/:id/edit" component={ EditMovie } />
-        <Route path="/" component={ MovieList } />
+        <Route path="/movies/:id" component={ MovieDetails } />
+        <Route exact path="/" component={ MovieList } />
         <Route path="" component={ NotFound } />
       </Switch>
     </BrowserRouter>
