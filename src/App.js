3c1f61d2f57e256c6ac8d99845ff178a735659ac
Ivaldo Souza <souza1976ivaldo@gmail.com>
2021-06-09 16:27:16 -0300

    Requisitos corrigidos

--- src/App.js
+++ src/App.js
@@ -16,12 +16,14 @@ class App extends Component {
           <Route exact path="/" component={ MovieList } />
           <Route path="/movies/new" component={ NewMovie } />
           <Route
-            path="/movies/:id"
-            render={ (props) => <MovieDetails { ...props } /> }
-          />
-          <Route
+            exact
             path="/movies/:id/edit"
             render={ (props) => <EditMovie { ...props } /> }
+          />
+          <Route
+            exact
+            path="/movies/:id"
+            render={ (props) => <MovieDetails { ...props } /> }
           />
           <Route path="*" component={ NotFound } />
         </Switch>
