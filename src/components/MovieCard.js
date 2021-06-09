c5c455c8b38f40f00b3740dd3ecdb7048dd8f1b0
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-06-09 18:28:45 -0300

    Requisito 3 - cria Link em movieCard

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, storyline, imagePath } = movie;
+    const { title, storyline, imagePath, id } = movie;
 
     return (
       <div data-testid="movie-card">
         <img src={ imagePath } alt="Movie Cover" />
         <h4>{ title }</h4>
         <p>{ storyline }</p>
+        <Link to={ `/movies/${id}` }>VER DETALHES</Link>
       </div>
     );
   }
@@ -21,6 +23,7 @@ MovieCard.propTypes = {
     title: PropTypes.string.isRequired,
     storyline: PropTypes.string.isRequired,
     imagePath: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
   }),
 };
 
