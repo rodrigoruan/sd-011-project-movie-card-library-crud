8723e45e0e9e6ef81514aa1ca9b1328941b0b66f
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-06-09 14:59:59 -0300

    Retira informaçoes adicionais do movieCard

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -4,18 +4,13 @@ import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
-    const { movie: { title, subtitle, storyline, rating,
-      imagePath, bookmarked, genre, id } } = this.props;
+    const { movie: { title, subtitle, storyline, id } } = this.props;
 
     return (
       <div data-testid="movie-card">
         <h4 className="movie-card-title">{ title }</h4>
         <h5 className="movie-card-subtitle">{ subtitle }</h5>
         <p className="movie-card-storyline">{ storyline }</p>
-        <h5 className="movie-card-rating">{ rating }</h5>
-        <h5 className="movie-card-bookmarked">{ bookmarked }</h5>
-        <h5 className="movie-card-genre">{ genre }</h5>
-        <img className="movie-card-image" src={ imagePath } alt="Folder Film" />
         <nav>
           <Link to={ `/movies/${id}` }>VER DETALHES</Link>
         </nav>
@@ -29,10 +24,6 @@ MovieCard.propTypes = {
     title: PropTypes.string,
     subtitle: PropTypes.string,
     storyline: PropTypes.string,
-    rating: PropTypes.number,
-    bookmarked: PropTypes.string,
-    genre: PropTypes.string,
-    imagePath: PropTypes.string,
     id: PropTypes.number,
   }).isRequired,
 };
