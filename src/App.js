72b1d24a68eaef6e8fe86d456f21e0765bb9bfff
Pedro Castro <pa.coelho.castro@gmail.com>
2021-06-09 10:19:54 -0300

    Req 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+
+import MovieList from './pages/MovieList';
+import NewMovie from './pages/NewMovie';
+import MovieDetails from './pages/MovieDetails';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <Router>
       <div>Movie Card Library CRUD</div>
+      <Route path="/">
+        <MovieList />
+      </Route>
+      <Route path="/movies/new">
+        <NewMovie />
+      </Route>
+      <Route path="/movies/:id">
+        <MovieDetails />
+      </Route>
+      <Route path="/movies/:id/edit">
+        <EditMovie />
+      </Route>
+      <Route path="*">
+        <NotFound />
+      </Route>
     </Router>
   );
 }
