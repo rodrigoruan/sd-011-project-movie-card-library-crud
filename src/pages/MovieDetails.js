bca3007c8e89bd162c0bcb85753edd1215dc76f4
Dário <dario.junior.rs@gmail.com>
2021-06-09 20:44:33 -0300

    Addicted login in Movie Details

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,23 +1,51 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
-  render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
+  constructor({ match }){
+    super({ match })
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    this.state = {
+      movieId: match.params.id,
+      movie: []
+    }
+
+    this.fetchMovieInformations = this.fetchMovieInformations.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchMovieInformations(this.state.movieId)
+  }
+
+  async fetchMovieInformations(movieId) {
+    const data = await movieAPI.getMovie(movieId);
+    this.setState({ movie: data });
+  }
+
+  render() {
+    const { movie, movieId } = this.state;
+    const { title, storyline, imagePath, genre, rating, subtitle } = movie;
+    
+    if (movie.length === 0) return <Loading />;
 
     return (
       <div data-testid="movie-details">
         <h1>Estou no movie details</h1>
         <img alt="Movie Cover" src={ `../${imagePath}` } />
+        <p>{ `Title: ${title}` }</p>
         <p>{ `Subtitle: ${subtitle}` }</p>
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
+        <button type="button">
+          <Link to={`/movie/${movieId}/edit`} >EDITAR</Link>
+        </button>
+        <button type="button">
+          <Link to="/" >VOLTAR</Link>
+        </button>
       </div>
     );
   }
