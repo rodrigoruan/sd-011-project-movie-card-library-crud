e10d50beb057863f2afc7704c33f94d9f0b261d5
Dário <dario.junior.rs@gmail.com>
2021-06-09 13:39:56 -0300

    sending props informations to movie card

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
 class MovieCard extends React.Component {
+  constructor(){
+    super()
+
+  }
   render() {
+    const { id,
+      title,
+      subtitle,
+      storyline,
+      rating,
+      imagePath,
+      gender,
+      bookmarked } = this.props.movie;
     return (
       <div data-testid="movie-card">
         Movie Card
