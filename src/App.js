9f299d60a2991b9715a7c40702ec845b091fe9d7
jeferson casagrande <jefersoncasagrandedesouza@gmail.com>
2021-06-09 19:25:29 -0300

    requisito 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
+import {
+  MovieList,
+  NewMovie,
+  MovieDetails,
+  EditMovie,
+  NotFound,
+} from './pages';
 
 function App() {
   return (
-    <Router>
-      <div>Movie Card Library CRUD</div>
-    </Router>
+    <div>
+      Movie Card Library CRUD
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ MovieList } />
+          <Route path="/movies/new" component={ NewMovie } />
+          <Route path="/movies/:id/edit" component={ EditMovie } />
+          <Route path="/movies/:id" component={ MovieDetails } />
+          <Route path="*" component={ NotFound } />
+        </Switch>
+      </BrowserRouter>
+    </div>
   );
 }
 
