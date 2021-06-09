f597cf8a85cf842a4e23eff08fd04fcd65a6f4a9
Dário <dario.junior.rs@gmail.com>
2021-06-09 13:24:56 -0300

    Add loading component before API results in movie list

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
 
 import * as movieAPI from '../services/movieAPI';
+import Loading from '../components/Loading';
+import movies from '../services/movieData';
 
 class MovieList extends Component {
   constructor() {
@@ -12,14 +14,19 @@ class MovieList extends Component {
     };
   }
 
+  async componentDidMount() {
+    const data = await movieAPI.getMovies();
+    this.setState({movies: data})
+  }
+
   render() {
     const { movies } = this.state;
-
-    // Render Loading here if the request is still happening
+    let loading = movies.length === 0;
 
     return (
       <div data-testid="movie-list">
         <h1>Estou no Movie List</h1>
+        { loading ? <Loading /> : null }
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
       </div>
     );
