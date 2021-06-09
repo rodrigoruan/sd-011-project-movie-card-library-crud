46ac02b5915df5468062b0fba1e4349c7c0fa4fe
Raphael <raphaellfpg@gmail.com>
2021-06-09 17:46:45 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import NewMovie from './pages/NewMovie';
+import MovieDetails from './pages/MovieDetails';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
-function App() {
-  return (
-    <div>Movie Card Library CRUDD</div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Route exact path="/" component={ MovieList } />
+
+        <Route exact path="/movies/new" component={ NewMovie } />
+
+        <Route exact path="/movies/:id" component={ MovieDetails } />
+
+        <Route exact path="/movies/:id/edit" component={ EditMovie } />
+
+        <Route component={ NotFound } />
+
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
+import Loading from '../components/Loading';
 
 import * as movieAPI from '../services/movieAPI';
 
@@ -15,7 +16,7 @@ class MovieList extends Component {
   render() {
     const { movies } = this.state;
 
-    // Render Loading here if the request is still happening
+    // Render Loading here if the rLOequest is still happening
 
     return (
       <div data-testid="movie-list">
