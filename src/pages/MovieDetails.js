2603be7f8e08ae17141aaa011d331c54e1d5b878
Tulio Sploradori <tuliospl@gmail.com>
2021-06-09 10:27:09 -0300

    conclusao do item 4 e 7

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import Loading from './components/Loading';
 import * as movieAPI from '../services/movieAPI';
 
@@ -10,6 +12,7 @@ class MovieDetails extends Component {
       movie: {},
     };
     this.listaMovie = this.listaMovie.bind(this);
+    this.deleta = this.deleta.bind(this);
   }
 
   componentDidMount() {
@@ -17,13 +20,23 @@ class MovieDetails extends Component {
   }
 
   listaMovie = async () => {
-    const filme = await movieAPI.getMovie(); // parametro de getmovie e a url que e o id
+    const { match } = this.props;
+    const { id } = match.params;
+    const filme = await movieAPI.getMovie(id);
     this.setState({ movie: filme, loading: false });
+  }
+
+  deleta() {
+    const { match } = this.props;
+    const { id } = match.params;
+    movieAPI.deleteMovie(id);
   }
 
   render() {
     const { loading, movie } = this.state;
     const { title, storyline, imagePath, genre, rating, subtitle } = movie;
+    const { match } = this.props;
+    const { id } = match.params;
     console.log(imagePath);
     if (loading) return <Loading />;
     return (
@@ -34,9 +47,17 @@ class MovieDetails extends Component {
         <div>{ `Storyline: ${storyline}` }</div>
         <div>{ `Genre: ${genre}` }</div>
         <div>{ `Rating: ${rating}` }</div>
+        <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
+        <Link to="/">VOLTAR</Link>
+        <Link to="/" onClick={ this.deleta }>DELETAR</Link>
       </div>
     );
   }
 }
+
+MovieDetails.propTypes = {
+  match: PropTypes.objectOf(PropTypes.array).isRequired,
+  id: PropTypes.string.isRequired,
+};
 
 export default MovieDetails;
