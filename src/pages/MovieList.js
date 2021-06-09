ba9e75946b4ad7a10df1d8012a48052b25dc669a
Wilquer82 <wilquer.fig@gmail.com>
2021-06-09 16:47:13 -0300

    1?

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,25 +1,39 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import MovieCard from '../components/MovieCard';
-
+import Loading from '../components/Loading';
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
   constructor() {
     super();
-
     this.state = {
       movies: [],
+      loading: false,
     };
   }
 
+  fetchAPI = () => {
+    this.setState({ loading: true }, async () => {
+      const movies = await movieAPI.getMovies();
+      this.setState({
+        movies,
+        loading: false,
+      });
+    });
+  };
+
+  componentDidMount = () => {
+    this.fetchAPI();
+  };
+
   render() {
-    const { movies } = this.state;
-
-    // Render Loading here if the request is still happening
-
+    const { movies, loading } = this.state;
+    if (loading) return <Loading />;
     return (
       <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        <Link to="/movies/new">ADICIONAR CARTÃO</Link>
       </div>
     );
   }
