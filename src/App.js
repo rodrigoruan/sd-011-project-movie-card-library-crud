b2020f0ab9cd2c0c37499838f785ae65eb7711b4
Bruno Davi <brunodaviandrade2000@gmail.com>
2021-06-09 08:00:52 -0300

    Vamo com tudo!!!

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import './App.css';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <BrowserRouter>
+      <Route>
+        Teste
+      </Route>
+    </BrowserRouter>
   );
 }
 
