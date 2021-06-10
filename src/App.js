ee41eac9d2cd28061ca853449ab63004f7201ad7
DADA <davimarque2000@gmail.com>
2021-06-10 18:23:52 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import './App.css';
+import { EditMovie, MovieList, MovieDetails, NewMovie,
+  NotFound } from './pages/index';
+import Title from './components/Title';
 
 function App() {
   return (
-    <Router>
-      <div>Movie Card Library CRUD</div>
-    </Router>
+    <div>
+      <Title />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ MovieList } />
+          <Route exact path="/movies/new" component={ NewMovie } />
+          <Route exact path="/movies/:id" component={ MovieDetails } />
+          <Route exact path="/movies/:id/edit" component={ EditMovie } />
+          <Route path="/404" component={ NotFound } />
+          <Redirect to="/404" />
+        </Switch>
+      </BrowserRouter>
+    </div>
   );
 }
 
---
+++ src/components/Title.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+class Title extends Component {
+  render() {
+    return <h1>Movie Card Library CRUD</h1>;
+  }
+}
+export default Title;
