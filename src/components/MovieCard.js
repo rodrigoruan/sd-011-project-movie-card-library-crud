38c63f0fe133ecd2351ac584e9039ec870c4cb6e
Daniel Leon <daniel.radialista@gmail.com>
2021-06-10 16:16:39 -0300

    acrescentando subtitulo no MovieCard

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -5,11 +5,12 @@ import PropTypes from 'prop-types';
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, storyline, id } = movie;
+    const { title, subtitle, storyline, id } = movie;
     return (
       <div data-testid="movie-card">
         Movie Card
-        <p>{ title }</p>
+        <h2>{ title }</h2>
+        <p>{ subtitle }</p>
         <p>{ storyline }</p>
         <Link to={ `movies/${id}` }>VER DETALHES</Link>
       </div>
@@ -20,6 +21,7 @@ class MovieCard extends React.Component {
 MovieCard.propTypes = {
   movie: PropTypes.shape({
     title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
     storyline: PropTypes.string.isRequired,
     id: PropTypes.number.isRequired,
   }).isRequired,
