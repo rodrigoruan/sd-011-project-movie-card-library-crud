96b767c27f9d51cc1378f9fd149fc39e4f36cbf1
fernando lasmar <f3rnandolasmar@gmail.com>
2021-06-09 23:09:47 -0300

    requisito 4

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         <Switch>
           <Route exact path="/" render={ () => <MovieList /> } />
           <Route path="/movies/new" render={ () => <NewMovie /> } />
-          <Route exact path="/movies/:id" render={ () => <MovieDetails /> } />
+          <Route exact path="/movies/:id" render={ (props) => <MovieDetails { ...props } /> } />
           <Route path="/movies/:id/edit" render={ () => <EditMovie /> } />
           <Route path="/*" render={ () => <NotFound /> } />
         </Switch>
