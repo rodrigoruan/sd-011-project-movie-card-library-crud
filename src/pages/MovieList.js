cfa623ba0065d21b673bef925d0167acde3edc32
guinhoanjos <iago.anjos@hotmail.com>
2021-06-10 19:46:18 -0300

    requisito 6

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import MovieCard from '../components/MovieCard';
 import Loading from '../components/Loading';
 import * as movieAPI from '../services/movieAPI';
@@ -6,7 +7,6 @@ import * as movieAPI from '../services/movieAPI';
 class MovieList extends Component {
   constructor() {
     super();
-
     this.state = {
       movies: '',
     };
@@ -24,12 +24,15 @@ class MovieList extends Component {
     // Render Loading here if the request is still happening
 
     return (
-      <div data-testid="movie-list" className="movie-list">
-        { movies ? (
-          movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)
-        ) : (
-          <Loading />
-        ) }
+      <div data-testid="movie-list">
+        <Link to="/movies/new">ADICIONAR CARTÃO</Link>
+        <div className="movie-list">
+          { movies ? (
+            movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)
+          ) : (
+            <Loading />
+          ) }
+        </div>
       </div>
     );
   }
