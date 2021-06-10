594b455952a4a5dc5b69896050eed32f70f9a626
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2021-06-10 17:44:29 -0300

    MovieCard.js| Req 3: add Link

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const {
+      movie: { id, title, subtitle, storyline },
+    } = this.props;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <h5>{ title }</h5>
+        <p>{ subtitle }</p>
+        <textarea defaultValue={ storyline } />
+        <Link to={ `movies/${id}` }>VER DETALHES</Link>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    id: PropTypes.number,
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+  }).isRequired,
+};
 
 export default MovieCard;
