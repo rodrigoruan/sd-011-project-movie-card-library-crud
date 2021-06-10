3f35d510dc4002997e4e02740aa55981b285896b
marsleite <marsleite@gmail.com>
2021-06-10 10:18:45 -0300

    requisito 03 e requisito 04 - done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css';
 import { BrowserRouter, Route } from 'react-router-dom';
 import MovieList from './pages/MovieList';
 import NewMovie from './pages/NewMovie';
@@ -10,9 +11,13 @@ function App() {
   return (
     <BrowserRouter>
       <div>Movie Card Library CRUD</div>
-      <Route path="/" component={ MovieList } />
+      <Route exact path="/" component={ MovieList } />
+      <Route
+        exact
+        path="/movies/:id"
+        render={ (props) => <MovieDetails { ...props } /> }
+      />
       <Route exact path="/movies/new" component={ NewMovie } />
-      <Route exact path="/movies/:id" component={ MovieDetails } />
       <Route exact path="/movies/:id/edit" component={ EditMovie } />
       <Route path="*" component={ NotFound } />
     </BrowserRouter>
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,22 +1,56 @@
 import React, { Component } from 'react';
 
+import { Link } from 'react-router-dom';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      movie: {},
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    this.gettingMovieApi();
+  }
+
+  async gettingMovieApi() {
+    const { match } = this.props;
+    const { id } = match.params;
+    this.setState(
+      { loading: true },
+      async () => {
+        const requestRtn = await movieAPI.getMovie(id);
+        this.setState({
+          movie: requestRtn,
+          loading: false,
+        });
+      },
+    );
+  }
+
   render() {
     // Change the condition to check the state
-    // if (true) return <Loading />;
-
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    const { movie, loading } = this.state;
+    if (loading) return <Loading />;
+    const { title, storyline, imagePath, genre, rating, subtitle, id } = movie;
 
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={ `../${imagePath}` } />
+        <p>{ `Title: ${title}` }</p>
         <p>{ `Subtitle: ${subtitle}` }</p>
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
+        <div>
+          <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
+        </div>
+        <Link to="/">VOLTAR</Link>
       </div>
     );
   }
