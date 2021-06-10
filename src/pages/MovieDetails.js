929a1defb335c57d19a7d8b69d26059319fed900
OldRoge <rogeriomrjr@icloud.com>
2021-06-10 18:42:57 -0300

    Requirement 4

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,27 +1,70 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-// import * as movieAPI from '../services/movieAPI';
-// import { Loading } from '../components';
+import * as movieAPI from '../services/movieAPI';
+import { Loading } from '../components';
 
 class MovieDetails extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      movie: {},
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    this.fetchMovie();
+  }
+
+  async fetchMovie() {
+    const { match: { params: { id } } } = this.props;
+
+    const movie = await movieAPI.getMovie(parseInt(id, 10));
+
+    this.setState({
+      loading: false,
+      movie,
+    });
+  }
+
   render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
-
-    // title
-
-    const { storyline, imagePath, genre, rating, subtitle } = {};
+    const {
+      loading,
+      movie: { storyline, imagePath, genre, rating, subtitle, title, id },
+    } = this.state;
 
     return (
       <div data-testid="movie-details">
-        <img alt="Movie Cover" src={ `../${imagePath}` } />
-        <p>{ `Subtitle: ${subtitle}` }</p>
-        <p>{ `Storyline: ${storyline}` }</p>
-        <p>{ `Genre: ${genre}` }</p>
-        <p>{ `Rating: ${rating}` }</p>
+        {
+          loading
+            ? <Loading />
+            : (
+              <section>
+                <img alt="Movie Cover" src={ `../${imagePath}` } />
+                <span>{ title }</span>
+                <p>{ `Subtitle: ${subtitle}` }</p>
+                <p>{ `Storyline: ${storyline}` }</p>
+                <p>{ `Genre: ${genre}` }</p>
+                <p>{ `Rating: ${rating}` }</p>
+                <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
+                <Link to="/">VOLTAR</Link>
+              </section>
+            )
+        }
       </div>
     );
   }
 }
 
 export default MovieDetails;
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }).isRequired,
+};
