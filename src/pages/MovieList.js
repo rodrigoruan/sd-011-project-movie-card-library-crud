8774f914523b457f0aa9bed4e18c924abe4bb8bb
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-06-10 17:28:35 -0300

    Adiciona link de adicionar cartao em MovieList

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import MovieCard from '../components/MovieCard';
 import Loading from '../components/Loading';
 import * as movieAPI from '../services/movieAPI';
@@ -38,6 +39,9 @@ class MovieList extends Component {
             movie={ movie }
           />))
           : <Loading />}
+        <section className="link">
+          <Link to="/movies/new">ADICIONAR CARTÃO</Link>
+        </section>
       </div>
     );
   }
