a4558eb16dfe0f609cf7143182dbd791f8a33125
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-06-10 17:50:13 -0300

    Adiciona requisito 6 - esse foi tranquilo!

--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
-
+import { Redirect } from 'react-router-dom';
 import MovieForm from '../components/MovieForm';
 import * as movieAPI from '../services/movieAPI';
 
 class NewMovie extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
+    this.state = {
+      shouldRedirect: false,
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleSubmit(newMovie) {
+  async handleSubmit(newMovie) {
+    this.setState({
+      shouldRedirect: true,
+    });
+    const movie = await movieAPI.createMovie(newMovie);
+
+    return movie;
   }
 
   render() {
+    const { shouldRedirect } = this.state;
+    if (shouldRedirect) {
+      return <Redirect to="/" />;
+    }
+
     return (
       <div data-testid="new-movie">
         <MovieForm onSubmit={ this.handleSubmit } />
