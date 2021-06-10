02b74afdd981fa83d799fc5b8712e070899e18d4
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-06-09 22:28:19 -0300

    Requisito 6 - Link e requisicao para criar novos cartoes

--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
-
+import { Redirect } from 'react-router-dom';
 import MovieForm from '../components/MovieForm';
-// import * as movieAPI from '../services/movieAPI';
+import * as movieAPI from '../services/movieAPI';
 
 class NewMovie extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      shouldRedirect: false,
+    };
+
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleSubmit(/* newMovie */) {
+  async handleSubmit(movieData) {
+    await movieAPI.createMovie(movieData);
+    this.setState({
+      shouldRedirect: true,
+    });
   }
 
   render() {
+    const { shouldRedirect } = this.state;
+
+    if (shouldRedirect === true) {
+      return <Redirect to="/" />;
+    }
+
     return (
       <div data-testid="new-movie">
         <MovieForm onSubmit={ this.handleSubmit } />
@@ -20,4 +34,5 @@ class NewMovie extends Component {
     );
   }
 }
+
 export default NewMovie;
