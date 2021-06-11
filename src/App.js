a38c63fe1e36b38ad23028ad6ce831b8b60bd71d
Rafael Schmitz <schimitaum@gmail.com>
2021-06-11 02:20:35 -0300

    req 3

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ import './App.css';
 function App() {
   return (
     <BrowserRouter>
-      <div>Movie Card Library CRUD</div>
       <Switch>
         <Route path="/movies/new" component={ NewMovie } />
         <Route path="/movies/:id/edit" component={ EditMovie } />
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const path = `movies/${movie.id}`;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <p>{movie.title}</p>
+        <p>{movie.storyline}</p>
+
+        <Link to={ path }> VER DETALHES </Link>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.exact().isRequired,
+};
 
 export default MovieCard;
