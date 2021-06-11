cf219ced524e76ec89694ff2d7852f2d3ae8a555
Paulo Henrique de Paula Bomfim <paulopbomfim@gmail.com>
2021-06-10 22:18:29 -0300

    Requisito 6 concluido - Pagina adicionar cartão implementada

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import MovieCard from '../components/MovieCard';
 import Loading from '../components/Loading';
 
@@ -29,6 +30,7 @@ class MovieList extends Component {
 
     return (
       <div data-testid="movie-list">
+        <Link to="/movies/new">ADICIONAR CARTÃO</Link>
         {
           movies.length === 0
             ? <Loading />
