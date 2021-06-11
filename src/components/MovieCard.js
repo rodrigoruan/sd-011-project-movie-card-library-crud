b6b39b7c10a99c8442df58ea31c1348a10e8435d
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2021-06-11 12:14:42 -0300

    exercicio 3 resolvido

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie: { storyline, id, title } } = this.props;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <p>{ title }</p>
+        <p>{ storyline }</p>
+        <Link to={ `/movies/${id}` } params={ id }> VER DETALHES </Link>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    storyline: PropTypes.string,
+    id: PropTypes.string,
+    title: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default MovieCard;
