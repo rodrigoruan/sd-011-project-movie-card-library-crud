4ac01b97b87992ebdd2b0abc59257feea060e4e3
Vinicius <viniciusfod@gmail.com>
2021-06-11 15:48:15 -0300

    requisito 5

--- src/App.js
+++ src/App.js
@@ -12,10 +12,10 @@ function App() {
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={ MovieList } />
-        <Route exact path="/movies/new" component={ NewMovie } />
-        <Route path="/movies/:id" render={ (props) => <MovieDetails { ...props } /> } />
-        <Route exact path="/movies/:id/edit" component={ EditMovie } />
-        <Route path="*" component={ NotFound } />
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route path="/movies/:id/edit" component={ EditMovie } />
+        <Route path="/movies/:id" component={ MovieDetails } />
+        <Route component={ NotFound } />
       </Switch>
     </BrowserRouter>
   );
