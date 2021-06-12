8212c9e4181b1038b91ee90c9f7325389a587921
Michelle <mls.srbek@gmail.com>
2021-06-11 21:43:02 -0300

    req 4 corrections

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ function App() {
         <Switch>
           <Route exact path="/" component={ MovieList } />
           <Route path="/movies/new" component={ NewMovie } />
-          <Route path="/movies/:id" component={ MovieDetails } />
-          <Route path="/movies/:id/edit" component={ EditMovie } />
+          <Route exact path="/movies/:id" component={ MovieDetails } />
+          <Route exact path="/movies/:id/edit" component={ EditMovie } />
           <Route path="*" component={ NotFound } />
         </Switch>
       </BrowserRouter>
