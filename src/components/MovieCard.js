f634bd069900d01e73788e1223595c7325ed8432
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-06-12 04:53:07 -0300

    task 3 1.0

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
-    // const { movie } = this.props;
-    // const { title, storyline, imagePath, id } = movie;
+    const { movie } = this.props;
+    const { title, storyline, imagePath, id } = movie;
 
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <img alt="Movie Cover" src={ imagePath } />
+        <div>
+          <h4>{title}</h4>
+          <p>{storyline}</p>
+        </div>
+        <div>
+          <Link to={ `/movies/${id}` }>VER DETALHES</Link>
+        </div>
       </div>
     );
   }
