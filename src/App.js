4ccee9c10f18152ccc950464b00fff676b79ec33
Michelle <mls.srbek@gmail.com>
2021-06-11 22:42:21 -0300

    req 1 corrections

--- src/App.js
+++ src/App.js
@@ -13,8 +13,15 @@ function App() {
         <Switch>
           <Route exact path="/" render={ (props) => <MovieList { ...props } /> } />
           <Route path="/movies/new" render={ (props) => <NewMovie { ...props } /> } />
-          <Route exact path="/movies/:id" render={ (props) => <MovieDetails { ...props } /> } />
-          <Route path="/movies/:id/edit" render={ (props) => <EditMovie { ...props } /> } />
+          <Route
+            exact
+            path="/movies/:id"
+            render={ (props) => <MovieDetails { ...props } /> }
+          />
+          <Route
+            path="/movies/:id/edit"
+            render={ (props) => <EditMovie { ...props } /> }
+          />
           <Route component={ NotFound } />
         </Switch>
       </BrowserRouter>
