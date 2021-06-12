fc16c02866572cac99e9f00c582728a79e2c3244
Rafael Schmitz <schimitaum@gmail.com>
2021-06-12 18:07:06 -0300

    req. 6

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import MovieCard from '../components/MovieCard';
 import Loading from '../components/Loading';
 import * as movieAPI from '../services/movieAPI';
@@ -28,11 +29,18 @@ class MovieList extends Component {
 
   render() {
     const { movies, loading } = this.state;
-
+    if (loading) {
+      return (
+        <div>
+          <Loading />
+        </div>
+      );
+    }
     return (
       <div data-testid="movie-list">
-        { loading ? <Loading />
-          : movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
+        <br />
+        <Link to="/movies/new"> ADICIONAR CARTÃO </Link>
       </div>
     );
   }
