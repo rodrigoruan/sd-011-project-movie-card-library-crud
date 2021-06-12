00290ee6992a717d94b14ee72b514730a9dcdf08
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-06-12 06:44:23 -0300

    NewMovie fixes

--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
-
 import { Redirect } from 'react-router';
 import MovieForm from '../components/MovieForm';
-
 import * as movieAPI from '../services/movieAPI';
 
 class NewMovie extends Component {
   constructor(props) {
     super(props);
 
+    this.state = { shouldRedirect: false };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
