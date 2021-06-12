ade75ca00ac8bf987b1a9a6489ecffc69c2ee584
Michelle <mls.srbek@gmail.com>
2021-06-11 22:21:19 -0300

    req 1 corrections

--- src/App.js
+++ src/App.js
@@ -9,13 +9,14 @@ import NotFound from './pages/NotFound';
 function App() {
   return (
     <div>
+      Movie Card Library CRUD
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={ MovieList } />
           <Route path="/movies/new" component={ NewMovie } />
           <Route exact path="/movies/:id" component={ MovieDetails } />
-          <Route exact path="/movies/:id/edit" component={ EditMovie } />
-          <Route><NotFound /></Route>
+          <Route path="/movies/:id/edit" component={ EditMovie } />
+          <Route component={ NotFound } />
         </Switch>
       </BrowserRouter>
     </div>
