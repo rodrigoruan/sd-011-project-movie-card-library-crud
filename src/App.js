8198ee6b4c84ced58a25ab81a211e4d44e32d6cf
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-06-12 04:24:49 -0300

    app.js structure - task 1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { MainRouter, Switch, Router } from 'react-router-dom';
 import './App.css';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import NotFound from './pages/NotFound';
+import EditMovie from './pages/EditMovie';
 
-// test
 function App() {
   return (
-    <Router>
+    <MainRouter>
       <div>Movie Card Library CRUD</div>
-    </Router>
+      <Switch>
+        <Router path="/" component={ MovieList } />
+        <Router path="/movies/new" component={ NewMovie } />
+        <Router path="/movies/:id/edit" component={ EditMovie } />
+        <Router cpath="/movies/:id" component={ MovieDetails } />
+        <Router component={ NotFound } />
+      </Switch>
+    </MainRouter>
   );
 }
 
