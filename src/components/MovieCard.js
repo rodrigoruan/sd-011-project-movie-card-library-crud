e279dfdebe851b86655de11951168e3e7aed7ac9
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-06-12 04:39:40 -0300

    MovieCard props

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+// import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    // const { movie } = this.props;
+    // const { title, storyline, imagePath, id } = movie;
+
     return (
       <div data-testid="movie-card">
         Movie Card
@@ -9,5 +14,16 @@ class MovieCard extends React.Component {
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    imagePath: PropTypes.string,
+    rating: PropTypes.number,
+    id: PropTypes.number,
+  }).isRequired,
+};
 
 export default MovieCard;
