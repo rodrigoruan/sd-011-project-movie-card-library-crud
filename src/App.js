e371c2189ded813effa3be74f54f8cbfadbcc274
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-06-12 06:46:28 -0300

    App.js exact path test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import './App.css';
-
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import MovieList from './pages/MovieList';
 import MovieDetails from './pages/MovieDetails';
@@ -13,7 +11,7 @@ function App() {
     <BrowserRouter>
       <div>Movie Card Library CRUD</div>
       <Switch>
-        <Route path="/" component={ MovieList } />
+        <Route exact path="/" component={ MovieList } />
         <Route path="/movies/new" component={ NewMovie } />
         <Route path="/movies/:id/edit" component={ EditMovie } />
         <Route path="/movies/:id" component={ MovieDetails } />
