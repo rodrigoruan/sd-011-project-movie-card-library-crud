9868fc648292f45b123fd4a5bd65db56dfd0e2a2
Leonardo Lima <lima.leonardo.r@gmail.com>
2021-06-13 19:34:54 -0300

    implementa o requisito 4

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,32 +1,82 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       isLoading: true,
-
+      movie: {
+        title: '',
+        storyline: '',
+        imagePath: '',
+        genre: '',
+        rating: 0,
+        subtitle: '',
+      },
     };
+
+    this.fetchMovie = this.fetchMovie.bind(this);
+    this.pageConstructor = this.pageConstructor.bind(this);
+  }
+
+  async componentDidMount() {
+    await this.fetchMovie();
+  }
+
+  async fetchMovie() {
+    const { params: { id } } = this.props.match;
+    const movie = await movieAPI.getMovie(id);
+
+    this.setState({
+      movie,
+      isLoading: false,
+    });
+  }
+
+  pageConstructor() {
+    const { params: { id } } = this.props.match;
+
+    const { movie: {
+      title,
+      storyline,
+      imagePath,
+      genre,
+      rating,
+      subtitle,
+    } } = this.state;
+
+    const { isLoading } = this.state;
+
+    if (!isLoading) {
+      return (
+        <div data-testid="movie-details">
+          <img alt="Movie Cover" src={ `../${imagePath}` } />
+          <p>{ `Title: ${title}` }</p>
+          <p>{ `Subtitle: ${subtitle}` }</p>
+          <p>{ `Storyline: ${storyline}` }</p>
+          <p>{ `Genre: ${genre}` }</p>
+          <p>{ `Rating: ${rating}` }</p>
+          <button type="button">
+            <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
+          </button>
+          <button type="button">
+            <Link to="/">VOLTAR</Link>
+          </button>
+        </div>
+      );
+    }
+    return <Loading isLoading={ isLoading } />;
   }
 
   render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
-    const { isLoading } = this.state;
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
-
     return (
-      <div data-testid="movie-details">
-        <Loading isLoading={ isLoading } />
-        <img alt="Movie Cover" src={ `../${imagePath}` } />
-        <p>{ `Subtitle: ${subtitle}` }</p>
-        <p>{ `Storyline: ${storyline}` }</p>
-        <p>{ `Genre: ${genre}` }</p>
-        <p>{ `Rating: ${rating}` }</p>
-      </div>
+
+      this.pageConstructor()
+
     );
   }
 }
