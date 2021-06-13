155c332e9c3e1b3d6c63024f8ce54f8b0cf45f48
Michel Pereira <michelbecauro@gmail.com>
2021-06-13 03:51:04 -0300

    hotfix/ESLint issues

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Redirect } from 'react-router-dom';
 import { MovieForm, Loading } from '../components';
 import * as movieAPI from '../services/movieAPI';
@@ -68,5 +69,11 @@ class EditMovie extends Component {
     );
   }
 }
+
+EditMovie.propTypes = {
+  match: PropTypes.shape({
+    params: { id: PropTypes.number },
+  }).isRequired,
+};
 
 export default EditMovie;
