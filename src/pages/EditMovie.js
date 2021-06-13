bbc819a97fdfe91b4448e8a3ee33ce22655e1d8f
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2021-06-12 21:35:18 -0300

    exercicio 5 pela metade

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,26 +1,34 @@
 import React, { Component } from 'react';
-
+import { Redirect } from 'react-router-dom';
+import Loading from '../components/Loading';
 import { MovieForm } from '../components';
 import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      status: 'loading',
+      movie: props,
+      shouldRedirect: false,
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleSubmit(updatedMovie) {
+    this.setState({
+      shouldRedirect: true,
+    });
   }
 
   render() {
     const { status, shouldRedirect, movie } = this.state;
     if (shouldRedirect) {
-      // Redirect
+      return <Redirect to="/" />;
     }
 
     if (status === 'loading') {
-      // render Loading
+      return <Loading />;
     }
 
     return (
