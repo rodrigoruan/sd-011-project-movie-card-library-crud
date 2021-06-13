9633a748bcc026026278f8583b62a9c91fe6095c
camiloLelis <lafirmafuerte@gmail.com>
2021-06-13 14:46:50 -0300

    requisito-1-tentativa-1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
 function App() {
   return (
     <Router>
       <div>Movie Card Library CRUD</div>
+      <Switch>
+        <Route exact path="/" />
+        <Route path="/movies/new" />
+        <Route path="/movies/:id" />
+        <Route path="/movies/:id/edit" />
+        <Route path="*" />
+      </Switch>
     </Router>
   );
 }
