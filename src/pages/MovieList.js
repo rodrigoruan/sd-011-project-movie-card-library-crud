a3dee223a388fe33280f83ce1e8d6559bf9d623b
Júlio Mota <juliomotacampos@gmail.com>
2021-06-13 15:54:53 -0300

    Adiciona exercicio 2

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
-
-// import * as movieAPI from '../services/movieAPI';
+import Loading from '../components/Loading';
+import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
   constructor() {
@@ -9,13 +9,34 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      loading: true,
     };
+
+    this.fetchMovie = this.fetchMovie.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchMovie();
+  }
+
+  async fetchMovie() {
+    this.setState(
+      { loading: true },
+      async () => {
+        const response = await movieAPI.getMovies();
+
+        this.setState({
+          movies: response,
+          loading: false,
+        });
+      },
+    );
   }
 
   render() {
-    const { movies } = this.state;
+    const { movies, loading } = this.state;
 
-    // Render Loading here if the request is still happening
+    if (loading) return <Loading />;
 
     return (
       <div data-testid="movie-list">
