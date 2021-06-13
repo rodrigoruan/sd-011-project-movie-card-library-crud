b3bde97e11ebea885d0337d9f84540629f72c77a
Júlio Mota <juliomotacampos@gmail.com>
2021-06-13 11:56:24 -0300

    Primeiro commit. Vamos com tuuudo! :rocket:

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-function App() {
-  return (
-    <div>Movie Card Library CRUD</div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <h1>Movie Card Library | CRUD</h1>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
