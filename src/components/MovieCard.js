5e5e139dba265c6309398b08af956e5cc9c9c56a
camiloLelis <lafirmafuerte@gmail.com>
2021-06-13 19:53:56 -0300

    requisito-3-tentat-1

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import PropType from 'prop-types';
+import { Link }from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie:{ title, storyline, imagePath, id }} = this.props;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <h1>{ title }</h1>
+        <p>{ storyline }</p>
+        <img src={ imagePath } alt={title}/>
+        <Link to={`/movie/${id}`}>VER DETALHES</Link>
       </div>
     );
   }
 }
+
+MovieCard.PropType = {
+  movie: PropType.shape({
+    title: PropType.string,
+    storyline: PropType.string,
+    id: PropType.number,
+    imagePath: PropType.string,
+  }).isRequired,
+};
 
 export default MovieCard;
