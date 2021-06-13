54d1a634cc2869307facf1016fadb8e697261038
camiloLelis <lafirmafuerte@gmail.com>
2021-06-13 15:24:23 -0300

    requisito-1-tentativa-3

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import './App.css';
-import { MovieList, NewMovie, MovieDetails, EditMovie, NotFound }  from './pages' 
+import { MovieList, NewMovie, MovieDetails, EditMovie, NotFound } from './pages';
 
 function App() {
   return (
     <Router>
       <div>Movie Card Library CRUD</div>
       <Switch>
-        <Route exact path="/"  component={ MovieList }/>
-        <Route path="/movies/new" component={ NewMovie }/>
+        <Route exact path="/" component={ MovieList } />
+        <Route path="/movies/new" component={ NewMovie } />
         <Route path="/movies/:id" component={ MovieDetails } />
-        <Route path="/movies/:id/edit" component={ EditMovie }/>
-        <Route path="*" component={ NotFound }/>
+        <Route path="/movies/:id/edit" component={ EditMovie } />
+        <Route path="*" component={ NotFound } />
       </Switch>
     </Router>
   );
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -23,5 +23,4 @@ class MovieDetails extends Component {
   }
 }
 
-
 export default MovieDetails;
