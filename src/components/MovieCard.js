0c52d07b0b4660db9c1f885d5b1c5c32130a57a5
Júlio Mota <juliomotacampos@gmail.com>
2021-06-13 16:17:28 -0300

    Renderiza title, storyLine e imagePath no MovieCard

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie: { title, storyline, imagePath } } = this.props;
+
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <h4>{ title }</h4>
+        <img src={ imagePath } alt={ `${title} cover art` } />
+        <p>{ storyline }</p>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    storyline: PropTypes.string,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
 
 export default MovieCard;
