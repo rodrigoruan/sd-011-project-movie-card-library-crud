ef7715148a867269db35022f75bc36d016f8d6d4
camiloLelis <lafirmafuerte@gmail.com>
2021-06-13 15:41:09 -0300

    requisito-1-tentativa-7

--- src/App.js
+++ src/App.js
@@ -7,12 +7,11 @@ import { MovieList, NewMovie, MovieDetails, EditMovie, NotFound } from './pages'
 function App() {
   return (
     <BrowserRouter>
-      <div>Movie Card Library CRUD</div>
       <Switch>
         <Route exact path="/" component={ MovieList } />
         <Route path="/movies/new" component={ NewMovie } />
-        <Route path="/movies/:id" component={ MovieDetails } />
         <Route path="/movies/:id/edit" component={ EditMovie } />
+        <Route path="/movies/:id" component={ MovieDetails } />
         <Route path="*" component={ NotFound } />
       </Switch>
     </BrowserRouter>
