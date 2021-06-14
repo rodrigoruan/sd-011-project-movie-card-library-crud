9e2453addffde7f4ede44c4cf5df8882f5ad38a7
Savio Moraes <smmsavio@hotmail.com>
2021-06-14 11:54:59 -0300

    test

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { getMovies } from '../services/movieAPI';
+// import { getMovies } from '../services/movieAPI';
 
 class MovieCard extends React.Component {
   render() {
     return (
-      <div className="movie-card" data-testid="movie-card">
-      </div>
+      <div className="movie-card" data-testid="movie-card"> </div>
     );
   }
 }
