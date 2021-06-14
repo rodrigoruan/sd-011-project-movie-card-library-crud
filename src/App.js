1535edf8e03beb0e2f17cc0f62ad38c540141856
Aline Hofner <alinehofner@gmail.com>
2021-06-13 22:37:06 -0300

    Altera app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import { MovieList, NewMovie, MovieDetails, EditMovie, NotFound } from './pages';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
+      <Link to="/movies/new">ADICIONAR CARTÃO</Link>
       <Switch>
-        <Route path="/" exact component={ MovieList } />
-        <Route path="/movies/new" exact component={ NewMovie } />
-        <Route path="/movies/:id" exact component={ MovieDetails } />
-        <Route path="/movies/:id/edit" exact component={ EditMovie } />
+        <Route path="/" exact component={ () => <MovieList /> } />
+        <Route path="/movies/new" exact component={ () => <NewMovie /> } />
+        <Route
+          exact
+          path="/movies/:id"
+          render={ (props) => <MovieDetails { ...props } /> }
+        />
+        <Route
+          exact
+          path="/movies/:id/edit"
+          render={ (props) => <EditMovie { ...props } /> }
+        />
         <Route component={ NotFound } />
       </Switch>
-    </Router>
+    </BrowserRouter>
   );
 }
 
