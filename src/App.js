4f19810686c2f6504a52291e1151abbfeceb0697
Valeriandreoni <valeriandreoni@gmail.com>
2021-06-14 08:28:09 -0300

    requisito 3

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React, { Component }from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import { MovieList, NewMovie, MovieDetails, EditMovie, NotFound } from './pages';
 
 class App extends Component {
-  render(){
+  render() {
     return (
-        <div>
-          <h1>Movie Card Library CRUD</h1>
-          <BrowserRouter>
-            <Switch>
-              <Route exact path="/" component= {MovieList}/>
-              <Route path="/movies/new" component= {NewMovie}/>
-              <Route exact path="/movies/:id" component= {MovieDetails}/>
-              <Route path="/movies/:id/edit" component= {EditMovie}/>
-              <Route path="*" component= {NotFound}/>
-            </Switch>
-          </BrowserRouter>
-        </div>
-      );
+      <div>
+        <h1>Movie Card Library CRUD</h1>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={ MovieList } />
+            <Route path="/movies/new" component={ NewMovie } />
+            <Route exact path="/movies/:id" component={ MovieDetails } />
+            <Route path="/movies/:id/edit" component={ EditMovie } />
+            <Route path="*" component={ NotFound } />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
   }
 }
-
 
 export default App;
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie: { title, imagePath, storyline, id, subtitle } } = this.props;
     return (
       <div data-testid="movie-card">
         Movie Card
+        <img src={ imagePath } alt={ title } />
+        <h1>{ title }</h1>
+        <h2>{subtitle}</h2>
+        <p>{ storyline }</p>
+        <Link to={ `movies/${id}` }>VER DETALHES</Link>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    imagePath: PropTypes.string,
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    id: PropTypes.number,
+  }).isRequired,
+};
 
 export default MovieCard;
