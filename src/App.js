3870c6642944312b1bf7da8d70aaa2f7218f3c43
Matheus Costa <mathws.costag@gmail.com>
2021-06-14 12:35:00 -0300

    Crias as rotas da pagina(Requisito 1)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,47 @@
 import React from 'react';
+import { Redirect, Route, Switch } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import NewMovie from './pages/NewMovie';
+import MovieDetails from './pages/MovieDetails';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <BrowserRouter>
+      <div>Movie Card Library CRUD</div>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          component={ MovieList }
+        />
+        <Route
+          exact
+          path="/movies/new"
+          component={ NewMovie }
+        />
+        <Route
+          exact
+          path="/movies/:id"
+          render={ (props) => <MovieDetails { ...props } /> }
+        />
+        <Route
+          exact
+          path="/movies/:id/edit"
+          render={ (props) => <EditMovie { ...props } /> }
+        />
+        <Route
+          exact
+          path="/NotFound"
+          component={ NotFound }
+        />
+        <Redirect
+          to="/NotFound"
+        />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -15,6 +15,7 @@ class NewMovie extends Component {
   render() {
     return (
       <div data-testid="new-movie">
+        teste
         <MovieForm onSubmit={ this.handleSubmit } />
       </div>
     );
