1defc7fcb47121b0d799870888cfec520733a8be
Cris Lustosa <cmlustosa@gmail.com>
2021-06-14 11:19:13 -0300

    Adiciona requisito 4

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,26 +1,66 @@
 import React, { Component } from 'react';
 
-// import * as movieAPI from '../services/movieAPI';
-// import { Loading } from '../components';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { getMovie } from '../services/movieAPI';
+import { Loading } from '../components';
 
 class MovieDetails extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      movie: {},
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    const { match } = this.props;
+    const { params } = match;
+    const { id } = params;
+
+    getMovie(id).then((movie) => {
+      this.bringDetails(movie);
+    });
+  }
+
+  bringDetails(movie) {
+    this.setState({
+      movie,
+      loading: false,
+    });
+  }
+
   render() {
     // Change the condition to check the state
     // if (true) return <Loading />;
+    const { movie, loading } = this.state;
+    if (loading) return <Loading />;
 
-    //  const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    const { title, storyline, imagePath, genre, rating, subtitle, id } = movie;
 
     return (
       <div data-testid="movie-details">
-        {/* <img alt="Movie Cover" src={ `../${imagePath}` } />
+        <img alt="Movie Cover" src={ `../${imagePath}` } />
+        <p>{`${title}`}</p>
         <p>{ `Subtitle: ${subtitle}` }</p>
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
-        <p>{ `Rating: ${rating}` }</p> */}
-        teste
+        <p>{ `Rating: ${rating}` }</p>
+        <Link to="/">VOLTAR</Link>
+        <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
       </div>
     );
   }
 }
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: {
+      id: PropTypes.number,
+    },
+  }).isRequired,
+};
 
 export default MovieDetails;
