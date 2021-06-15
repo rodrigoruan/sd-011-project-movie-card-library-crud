b75eaff86eb4d37c720eef9244292d3382fe129b
Ana Carolina R Santos <kroline0693@gmail.com>
2021-06-15 11:53:40 -0300

    Requisito 4

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,18 +1,45 @@
 import React, { Component } from 'react';
-
+import PropTypes from 'prop-types';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      movie: [],
+      loading: true,
+    };
+    this.getMovie = this.getMovie.bind(this);
+  }
+
+  componentDidMount() {
+    this.getMovie();
+  }
+
+  async getMovie() {
+    const { match } = this.props;
+    const { params } = match;
+    const { id } = params;
+    console.log(this.props);
+
+    const res = await movieAPI.getMovie(id);
+
+    this.setState({
+      movie: res,
+      loading: false,
+    });
+  }
+
   render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
+    const { movie: { title, storyline, imagePath, genre, rating, subtitle } } = this.state;
+    const { loading } = this.state;
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
-
-    return (
+    return loading ? <Loading /> : (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={ `../${imagePath}` } />
+        <p>{ title }</p>
         <p>{ `Subtitle: ${subtitle}` }</p>
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
@@ -21,5 +48,13 @@ class MovieDetails extends Component {
     );
   }
 }
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.number,
+    }),
+  }).isRequired,
+};
 
 export default MovieDetails;
