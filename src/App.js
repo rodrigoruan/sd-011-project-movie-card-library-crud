0d443575047bf6fc8dd445fdaca689d4f0f5e6ae
AlexSmith7192 <alexsmith7192@gmail.com>
2021-06-15 10:56:33 -0300

    1 - Renderiza BrowserRouter no componente App usando rotas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import NewMovie from './pages/NewMovie';
+import MovieDetails from './pages/MovieDetails';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <div>
+      <h1>Movie Card Library CRUD</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={ MovieList } />
+          <Route path="/movies/new" component={ NewMovie } />
+          <Route path="/movies/:id" component={ MovieDetails } />
+          <Route path="/movies/:id/edit" component={ EditMovie } />
+          <Route component={ NotFound } />
+        </Switch>
+      </BrowserRouter>
+    </div>
   );
 }
 
