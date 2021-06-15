ab85ea7e818887a35450740c1777e0a446c8f680
Cris Lustosa <cmlustosa@gmail.com>
2021-06-15 10:45:44 -0300

    Adiciona requisito 4 e 5

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,28 +1,56 @@
 import React, { Component } from 'react';
 
+import { Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import Loading from '../components/Loading';
 import { MovieForm } from '../components';
-// import * as movieAPI from '../services/movieAPI';
+import { updateMovie, getMovie } from '../services/movieAPI';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      status: 'loading',
+      shouldRedirect: false,
+      movie: {},
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.bringDetails = this.bringDetails.bind(this);
+  }
+
+  componentDidMount() {
+    const { match } = this.props;
+    const { params } = match;
+    const { id } = params;
+
+    getMovie(id).then((movie) => {
+      this.bringDetails(movie);
+    });
   }
 
   handleSubmit(updatedMovie) {
-    // depois remover o consolelog foi so para passar no lint
-    console.log(updatedMovie);
+    updateMovie(updatedMovie);
+    this.setState({
+      shouldRedirect: true,
+    });
+  }
+
+  bringDetails(movie) {
+    this.setState({
+      movie,
+      status: false,
+    });
   }
 
   render() {
     const { status, shouldRedirect, movie } = this.state;
     if (shouldRedirect) {
       // Redirect
+      return <Redirect to="/" />;
     }
 
     if (status === 'loading') {
-      // render Loading
+      return <Loading />;
     }
 
     return (
@@ -32,5 +60,13 @@ class EditMovie extends Component {
     );
   }
 }
+
+EditMovie.propTypes = {
+  match: PropTypes.shape({
+    params: {
+      id: PropTypes.number,
+    },
+  }).isRequired,
+};
 
 export default EditMovie;
