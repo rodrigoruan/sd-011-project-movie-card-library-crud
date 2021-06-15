26ff1457ff0621ebf49b78f7128681ad4156ef43
DADA <davimarque2000@gmail.com>
2021-06-15 14:14:55 -0300

    requisito 1 reparo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect, Link } from 'react-router-dom';
 import './App.css';
 import { EditMovie, MovieList, MovieDetails, NewMovie,
   NotFound } from './pages/index';
