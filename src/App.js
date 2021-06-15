c52810f765decad7a6728a643caecb680da2a29d
pmaiamateus <pmaiamateus@gmail.com>
2021-06-15 15:25:06 -0300

    adiciona passo 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import { MovieList, NewMovie, MovieDetails, EditMovie, NotFound } from './pages';
 
 function App() {
   return (
     <Router>
       <div>Movie Card Library CRUD</div>
+      <Switch>
+        <Route exact path='/' render={ () => <MovieList/> } />
+        <Route exact path='/movies/new' render={ () => <NewMovie /> } />
+        <Route exact path='/movies/:id' render={ () => <MovieDetails /> } />
+        <Route exact path='/movies/:id/edit' render={ () => <EditMovie/> } />
+        <Route NoMatch render={ () => <NotFound/> }/>
+      </Switch>
     </Router>
   );
 }
