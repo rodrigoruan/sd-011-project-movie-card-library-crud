a09be11426150ddb3f3b919284fd9b9e6a4e2da4
Matheus Costa <mathws.costag@gmail.com>
2021-06-15 09:56:25 -0300

    Implementa o component MovieCard

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const redirectLink = `/movies/${movie.id}`;
     return (
-      <div data-testid="movie-card">
-        Movie Card
+      <div data-testid="movie-card" id={ movie.id }>
+        <h2>{ movie.title }</h2>
+        <p>{ movie.storyline }</p>
+        <Link to={ redirectLink }>VER DETALHES</Link>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.isRequired,
+};
 
 export default MovieCard;
