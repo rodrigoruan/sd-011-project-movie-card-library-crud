327115f13f2d5429be39db0850a0dc4cc40caae0
AlexSmith7192 <alexsmith7192@gmail.com>
2021-06-16 10:26:45 -0300

    2 - Faz uma requisição para buscar e mostrar a lista de filmes quando MovieList for montado

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
+import Loading from '../components/Loading';
 
 import * as movieAPI from '../services/movieAPI';
 
@@ -8,18 +9,44 @@ class MovieList extends Component {
     super();
 
     this.state = {
+      loading: true,
       movies: [],
     };
+
+    this.fetchMovies = this.fetchMovies.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchMovies();
+  }
+
+  async fetchMovies() {
+    this.setState(
+      { loading: true },
+      async () => {
+        const response = await movieAPI.getMovies();
+        this.setState({
+          loading: false,
+          movies: response,
+        });
+      },
+    );
+  }
+
+  cards() {
+    const { movies } = this.state;
+
+    return (
+      movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)
+    );
   }
 
   render() {
-    const { movies } = this.state;
-
-    // Render Loading here if the request is still happening
+    const { loading } = this.state;
 
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        { loading ? <Loading /> : this.cards()}
       </div>
     );
   }
