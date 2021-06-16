175a6306b42365b2111012938afdd71293d4bcd6
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2021-06-16 08:46:06 -0300

    movieCard

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import MovieDetails from '../pages/MovieDetails';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <MovieDetails MovieInfos={ movie } />
+        <Link to="/movies/:id">VER DETALHES</Link>
       </div>
     );
   }
 }
 
 export default MovieCard;
+
+MovieCard.propTypes = {
+  movie: PropTypes.array,
+}.isRequired;
