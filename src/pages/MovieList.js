8581cc783ed6e59e4ba489c839eeec2796ec4ef7
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2021-06-16 09:06:19 -0300

    movieList

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
+import Loading from '../components/Loading';
+import { getMovies } from '../services/movieAPI';
 
 class MovieList extends Component {
   constructor() {
@@ -7,17 +9,31 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      verified: false,
     };
   }
 
+  componentDidMount() {
+    this.fetchGetMovies();
+  }
+
+  fetchGetMovies() {
+    getMovies()
+      .then((movieList) => {
+        this.setState({
+          movies: movieList,
+          verified: true,
+        });
+      });
+  }
+
   render() {
-    const { movies } = this.state;
-
     // Render Loading here if the request is still happening
-
+    const { movies, verified } = this.state;
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        { verified ? this.fetchGetMovies() : <Loading /> }
+        { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
       </div>
     );
   }
