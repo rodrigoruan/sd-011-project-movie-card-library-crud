6d784228ddd9562972fb4ec8a669a0a8882ba65c
Savio Moraes <smmsavio@hotmail.com>
2021-06-16 00:25:54 -0300

    tetting...

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,22 +1,38 @@
 import React, { Component } from 'react';
-
-// import * as movieAPI from '../services/movieAPI';
-// import { Loading } from '../components';
+import { Link } from 'react-router-dom';
+import { Loading } from '../components';
+import CardDetails from '../components/CardDetails';
+import * as movieAPI from '../services/movieAPI';
 
 class MovieDetails extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+    };
+    this.fetchMovie = this.fetchMovie.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchMovie();
+  }
+
+  async fetchMovie() {
+    await movieAPI.getMovie();
+    this.setState({
+      loading: false,
+    });
+  }
+
   render() {
+    const { loading } = this.state;
     // Change the condition to check the state
     // if (true) return <Loading />;
 
-    const { storyline, imagePath, genre, rating, subtitle } = {};
-
     return (
-      <div data-testid="movie-details">
-        <img alt="Movie Cover" src={ `../${imagePath}` } />
-        <p>{ `Subtitle: ${subtitle}` }</p>
-        <p>{ `Storyline: ${storyline}` }</p>
-        <p>{ `Genre: ${genre}` }</p>
-        <p>{ `Rating: ${rating}` }</p>
+      <div>
+        <Link to="/movies/:id/edit">EDITAR</Link>
+        <div>{loading ? <Loading /> : <CardDetails />}</div>
       </div>
     );
   }
