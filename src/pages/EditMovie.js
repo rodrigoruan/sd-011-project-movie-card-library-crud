6d9a038ad797c5a9b8d4bc625ddfcd35098a940f
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2021-06-16 14:43:42 -0300

    fim do tempo dedicado ao projeto

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -5,8 +5,31 @@ import { MovieForm } from '../components';
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      shouldRedirect: false,
+      status: 'loading',
+      movie: {},
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    this.mounted = true;
+    const { match } = this.props;
+    const { params } = match;
+    const { id } = params;
+
+    movieAPI.getMovie(id)
+      .then((movie) => {
+        if (this.mounted) {
+          this.setState({ movie, status: 'loaded' });
+        }
+      })
+      .catch((err) => console.error(err));
+  }
+
+  componentWillUnmount() {
+    this.mounted = false;
   }
 
   handleSubmit(updatedMovie) {
