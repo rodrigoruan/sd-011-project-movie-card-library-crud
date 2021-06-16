2add556efcaa75773da737064db40e26ae8cc24a
Michelle <mls.srbek@gmail.com>
2021-06-16 20:42:31 -0300

    req 4 corrections

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -43,7 +43,7 @@ class MovieDetails extends Component {
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
         <Link to="/">VOLTAR</Link>
-        <Link to={ `movies/${id}/edit` }>EDITAR</Link>
+        <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
       </div>
     );
   }
