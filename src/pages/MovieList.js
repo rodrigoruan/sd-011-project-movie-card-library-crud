aee316d34cccfe30d92960604703210546c28e3e
Michelle <mls.srbek@gmail.com>
2021-06-16 21:34:36 -0300

    req 6 - new movie component

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import MovieCard from '../components/MovieCard';
 import { Loading } from '../components';
 import * as movieAPI from '../services/movieAPI';
@@ -37,6 +38,8 @@ class MovieList extends Component {
         {loading ? <Loading /> : movies.map(
           (movie) => <MovieCard key={ movie.title } movie={ movie } />,
         )}
+
+        <Link to="/movies/new">ADICIONAR CARTÃO</Link>
       </div>
     );
   }
