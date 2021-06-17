9873e6b5cb9f34c4fe5f04321cccebb0df02d114
Diogo Bontempo <diogo_bontempo@hotmail.com>
2021-06-16 23:28:04 -0300

    exercicio 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NewMovie from './pages/NewMovie';
+import MovieDetails from './pages/MovieDetails';
+import MovieList from './pages/MovieList';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
+import './App.css';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <Router>
+      <Switch>
+        <Route path="/movies/:id/edit" component={ EditMovie } />
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route path="/movies/:id" component={ MovieDetails } />
+        <Route exact path="/" component={ MovieList } />
+        <Route path="*" component={ NotFound } />
+      </Switch>
+    </Router>
   );
 }
 
-exportt default App;
+export default App;
