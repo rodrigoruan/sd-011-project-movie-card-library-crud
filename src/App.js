9702833521d338b2901f9903fa2836129658cb13
heyynat <natalifplima@gmail.com>
2021-06-17 10:51:41 -0400

    Requisito 5

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends React.Component {
           <Route exact path="/"><MovieList/></Route>
           <Route exact path="/movies/new"><NewMovie/></Route>
           <Route exact path="/movies/:id" render={(props) => <MovieDetails {...props} />}></Route>
-          <Route exact path="/movies/:id/edit"><EditMovie/></Route>
+          <Route exact path="/movies/:id/edit" render={(props) => <EditMovie {...props} />}></Route>
           <Route path="*"><NotFound/></Route>
         </Switch>
       </BrowserRouter>
--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,31 +1,64 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router';
 
-import { MovieForm } from '../components';
+import { Loading, MovieForm } from '../components';
 import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      status: 'loading',
+      shouldRedirect: null, 
+      movie: [],
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.fecthApi = this.fecthApi.bind(this);
   }
-
+  
   handleSubmit(updatedMovie) {
+    this.setState(
+      { loading: 'loading' },
+    async () => {
+      this.setState({
+        status: 'succeful',
+        shouldRedirect: true,
+        movie: await movieAPI.updateMovie(updatedMovie)
+      });
+    });
   }
-
-  render() {
-    const { status, shouldRedirect, movie } = this.state;
-    if (shouldRedirect) {
-      // Redirect
+  
+  async fecthApi() {
+    const { id } = this.props.match.params
+    this.setState(
+      { loading: 'loading' },
+      async () => {
+        this.setState({
+          shouldRedirect: false,
+          status: 'loading',
+          movie: await movieAPI.getMovie(id)
+        });
+      });
     }
-
-    if (status === 'loading') {
-      // render Loading
+    
+    componentDidMount() {
+      this.fecthApi();
     }
+    
+    
+    render() {
+      const { status, shouldRedirect, movie } = this.state;
+      
+      if (shouldRedirect === false) {
+        return <MovieForm movie={ movie } onSubmit={ this.handleSubmit }/>
+      }
+      if (shouldRedirect) {
+       return <Redirect to="/" />
+      }
 
     return (
       <div data-testid="edit-movie">
-        <MovieForm movie={ movie } onSubmit={ this.handleSubmit } />
+        {status === 'loading'? <Loading /> : <MovieForm movie={ movie } onSubmit={ this.handleSubmit }/>}
       </div>
     );
   }
