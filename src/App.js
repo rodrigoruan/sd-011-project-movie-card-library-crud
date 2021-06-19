8c7c1358c4dcc9b4e43cf52ffcef9a5f26d5b0e8
AlexSmith7192 <alexsmith7192@gmail.com>
2021-06-18 21:43:38 -0300

    1 - Refatorando...

--- src/App.js
+++ src/App.js
@@ -8,18 +8,16 @@ import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <h1>Movie Card Library CRUD</h1>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={ MovieList } />
-          <Route exact path="/movies/new" component={ NewMovie } />
-          <Route exact path="/movies/:id" component={ MovieDetails } />
-          <Route exact path="/movies/:id/edit" component={ EditMovie } />
-          <Route component={ NotFound } />
-        </Switch>
-      </BrowserRouter>
-    </div>
+      <Switch>
+        <Route exact path="/" component={ MovieList } />
+        <Route exact path="/movies/new" component={ NewMovie } />
+        <Route exact path="/movies/:id" component={ MovieDetails } />
+        <Route exact path="/movies/:id/edit" component={ EditMovie } />
+        <Route component={ NotFound } />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
