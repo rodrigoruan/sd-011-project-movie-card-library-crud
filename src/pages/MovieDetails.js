cba94e2c8ee14698ab6077d32b48ed1c8e4c0ea8
camiloLelis <lafirmafuerte@gmail.com>
2021-06-20 19:40:37 -0300

    requisito-4-tentativa-2

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state= {
+    this.state = {
       loading: true,
       movie: {},
-    }
+    };
     this.fethApiSet = this.fethApiSet.bind(this);
   }
+
   componentDidMount() {
     this.fethApiSet();
   }
@@ -28,13 +30,13 @@ class MovieDetails extends Component {
         });
       },
     );
-  };
-  
+  }
+
   render() {
     // Change the condition to check the state
-    const { movie, loading } = this.state; 
+    const { movie, loading } = this.state;
     const { title, storyline, imagePath, genre, rating, subtitle, id } = movie;
-    
+
     if (loading) return <Loading />;
 
     return (
@@ -45,11 +47,19 @@ class MovieDetails extends Component {
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
-        <Link to={`/movies/${ id }/edit`}>EDITAR</Link>
+        <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
         <Link to="/">VOLTAR</Link>
       </div>
     );
   }
 }
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }).isRequired,
+  }).isRequired,
+};
 
 export default MovieDetails;
