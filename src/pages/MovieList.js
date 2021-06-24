e1865c98650fbace8c23e9d379d4546f2860691d
Taís Carvalho <taispcarvalho@gmail.com>
2021-06-24 16:14:39 -0300

    2 + lint

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 import MovieCard from '../components/MovieCard';
+import Loading from '../components/Loading';
+import * as movieAPI from '../services/movieAPI';
 
-// import * as movieAPI from '../services/movieAPI';
-
-class MovieList extends Component {
+class MovieList extends React.Component {
   constructor() {
     super();
 
@@ -12,14 +12,27 @@ class MovieList extends Component {
     };
   }
 
+  async componentDidMount() {
+    const movieList = await movieAPI.getMovies();
+    this.getMoviesData(movieList);
+  }
+
+  getMoviesData(movies) {
+    this.setState({
+      movies,
+    });
+  }
+
   render() {
     const { movies } = this.state;
-
-    // Render Loading here if the request is still happening
-
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        {movies.length === 0 ? <Loading />
+          : movies.map((movie) => (
+            <MovieCard
+              key={ movie.title }
+              movie={ movie }
+            />))}
       </div>
     );
   }
