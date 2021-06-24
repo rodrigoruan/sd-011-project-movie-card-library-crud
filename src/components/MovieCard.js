af1f38ab6a703af47e707847e765b18c38fae59e
Taís Carvalho <taispcarvalho@gmail.com>
2021-06-24 16:36:41 -0300

    3 + lint

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie: { title, genre, storyline, imagePath, id } } = this.props;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <h2>{ title }</h2>
+        <h3>{ genre }</h3>
+        <p>{ storyline }</p>
+        <img src={ imagePath } alt={ title } />
+        <Link to={ `movies/${id}` }>VER DETALHES</Link>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    genre: PropTypes.string,
+    storyline: PropTypes.string,
+    imagePath: PropTypes.string,
+    id: PropTypes.number,
+  }).isRequired,
+};
 
 export default MovieCard;
