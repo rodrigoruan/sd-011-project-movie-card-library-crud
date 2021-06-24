c6d1f4e1627b766e0114b4782596a8c595633f74
Taís Carvalho <taispcarvalho@gmail.com>
2021-06-24 15:32:22 -0300

    1 + lint

--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -1,23 +1,21 @@
 import React, { Component } from 'react';
 
-import MovieForm from '../components/MovieForm';
-import * as movieAPI from '../services/movieAPI';
+// import MovieForm from '../components/MovieForm';
+// import * as movieAPI from '../services/movieAPI';
 
 class NewMovie extends Component {
-  constructor(props) {
-    super(props);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleSubmit(newMovie) {
-  }
-
+  // constructor(props) {
+  //   super(props);
+  // this.handleSubmit = this.handleSubmit.bind(this);
+  // handleSubmit(newMovie) {
+  // }
   render() {
     return (
       <div data-testid="new-movie">
-        <MovieForm onSubmit={ this.handleSubmit } />
+        {/* <MovieForm onSubmit={ this.handleSubmit } /> */}
       </div>
     );
   }
 }
+
 export default NewMovie;
