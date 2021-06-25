78daedac0cae93bbc8059cebe8bbf81a8d09d75c
teofilobrandao <teofiloafssbrandao@gmail.com>
2021-06-25 19:40:04 -0300

    Projeto Movie Card Library Crud - Envio item 1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
+// Requisito resolvido com consultas ao sites:
+// https://app.betrybe.com/course/live-lectures/sd-cohort-8#aula-132-react-router
+// https://www.youtube.com/watch?v=9pB_lwmLc74
+
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import MovieList from './pages/MovieList';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import MovieDetails from './pages/MovieDetails';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <Router>
-      <div>Movie Card Library CRUD</div>
-    </Router>
+    <BrowserRouter>
+      <h1>Movie Card Library CRUD</h1>
+      <Switch>
+        <Route exact path="/" component={ MovieList } />
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route path="/movies/:id/edit" component={ EditMovie } />
+        <Route path="/movies/:id" component={ MovieDetails } />
+        <Route path="*" component={ NotFound } />
+      </Switch>
+    </BrowserRouter>
   );
 }
+// }
 
 export default App;
