56fadc66e25c247a678a6095d15b2330425b4b28
LuanAlexandre <luanrb19@gmail.com>
2021-06-25 15:32:00 -0400

    Dia 13.3 - projeto-movie-cards-library-crud: só vem ontrack

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div>Movie Card Library CRUD</div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div>Movie Card Library CRUD</div>
+    );
+  }
 }
-
-export default App;
