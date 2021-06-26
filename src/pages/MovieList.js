21fda751bba4c101bd6e2b155a75866fec13d2e8
teofilobrandao <teofiloafssbrandao@gmail.com>
2021-06-26 16:55:26 -0300

    Projeto Movie Card Library Crud - Envio item 2

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,7 +1,13 @@
+// Requisito resolvido com consultas ao sites baixo
+// e aos projetos Movie Card Library e Stateful:
+// https://app.betrybe.com/course/live-lectures/sd-cohort-8#aula-132-react-router
+// https://www.youtube.com/watch?v=6ZVUCiriOZ4
+// https://www.youtube.com/watch?v=h0sNAXE1ozo
+
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
-
-// import * as movieAPI from '../services/movieAPI';
+import Loading from '../components/Loading';
+import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
   constructor() {
@@ -9,14 +15,27 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      loading: true,
     };
+    this.filmsList = this.filmsList.bind(this);
+  }
+
+  componentDidMount() {
+    this.filmsList();
+  }
+
+  async filmsList() {
+    const listFilms = await movieAPI.getMovies();
+    this.setState({ movies: listFilms, loading: false });
   }
 
   render() {
-    const { movies } = this.state;
+    const { movies, loading } = this.state;
 
     // Render Loading here if the request is still happening
-
+    if (loading === true) {
+      return <Loading />;
+    }
     return (
       <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
