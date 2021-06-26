b6c8331d08b434613deb7f396d521fcd55ac9959
teofilobrandao <teofiloafssbrandao@gmail.com>
2021-06-26 14:50:49 -0300

    Projeto Movie Card Library Crud - Correçao item 1

--- src/components/MovieForm.js
+++ src/components/MovieForm.js
@@ -164,4 +164,27 @@ class MovieForm extends React.Component {
   }
 }
 
+MovieForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+    genre: PropTypes.string,
+  }),
+};
+
+MovieForm.defaultProps = {
+  movie: {
+    title: '',
+    subtitle: '',
+    storyline: '',
+    rating: 0,
+    imagePath: '',
+    genre: '',
+  },
+};
+
 export default MovieForm;
