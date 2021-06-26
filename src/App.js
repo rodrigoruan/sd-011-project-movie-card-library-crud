54560523f41287a8759cd4196023fc8234102673
teofilobrandao <teofiloafssbrandao@gmail.com>
2021-06-25 22:08:37 -0300

    Projeto Movie Card Library Crud - Correçao item 1

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import EditMovie from './pages/EditMovie';
 import MovieDetails from './pages/MovieDetails';
 import NotFound from './pages/NotFound';
 
-function App() {
+export default function App() {
   return (
     <BrowserRouter>
       <h1>Movie Card Library CRUD</h1>
@@ -25,6 +25,5 @@ function App() {
     </BrowserRouter>
   );
 }
-// }
 
-export default App;
+// export default App;
