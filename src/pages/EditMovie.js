2b39a5d490e6fe93227e6d08f757cbc46f1b8e99
teofilobrandao <teofiloafssbrandao@gmail.com>
2021-06-27 15:49:05 -0300

    Projeto Movie Card Library Crud - Envio item 5

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,26 +1,54 @@
 import React, { Component } from 'react';
-
+import propTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
+import Loading from '../components/Loading';
 import { MovieForm } from '../components';
-// import * as movieAPI from '../services/movieAPI';
+import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+
+    this.state = {
+      movie: {},
+      loading: true,
+      shouldRedirect: false,
+    };
+
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.filmDetail = this.filmDetail.bind(this);
   }
 
-  handleSubmit() {
+  componentDidMount() {
+    this.filmDetail();
+  }
+
+  handleSubmit(movie) {
+    this.setState({ shouldRedirect: true });
+    console.log(movie);
+    movieAPI.updateMovie(movie);
+  }
+
+  async filmDetail() {
+    const { match: { params: { id } } } = this.props;
+    console.log(id);
+    const movie = await movieAPI.getMovie(id);
+    this.setState({ movie, loading: false });
+    console.log(movie);
   }
 
   render() {
-    const { status, shouldRedirect, movie } = this.state;
+    const { loading, shouldRedirect, movie } = this.state;
     if (shouldRedirect) {
-      // Redirect
+      return <Redirect to="/" />;
     }
 
-    if (status === 'loading') {
-      // render Loading
+    // if (status === 'loading') {
+    //   // render Loading
+    // }
+
+    if (loading) {
+      return <Loading />;
     }
 
     return (
@@ -30,5 +58,13 @@ class EditMovie extends Component {
     );
   }
 }
+
+EditMovie.propTypes = {
+  match: propTypes.shape({
+    params: propTypes.shape({
+      id: propTypes.number,
+    }),
+  }).isRequired,
+};
 
 export default EditMovie;
