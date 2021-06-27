e54e84f66cd73717bc7423db99875631372734c7
camiloLelis <lafirmafuerte@gmail.com>
2021-06-27 19:21:01 -0300

    requisito-5e6-tentat-4

--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -7,17 +7,24 @@ import * as movieAPI from '../services/movieAPI';
 class NewMovie extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      redirect: false,
+    }
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   async handleSubmit( newMovie ) {
     const novoCadastroMovie = await movieAPI.createMovie(newMovie);
-    if(novoCadastroMovie === "ok") {
-      return <Redirect to ="/" />;
+    if(novoCadastroMovie === "OK") {
+      this.setState({
+        redirect: true,
+      });
     }
   }
 
   render() {
+     
+    if (this.state.redirect) return <Redirect to ="/" />;
     return (
       <div data-testid="new-movie">
         <MovieForm onSubmit={ this.handleSubmit } />
