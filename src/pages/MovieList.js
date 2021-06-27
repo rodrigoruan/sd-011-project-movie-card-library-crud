42ba51b6a9c703bc0728b151ab30daa00072d8cb
teofilobrandao <teofiloafssbrandao@gmail.com>
2021-06-27 16:17:32 -0300

    Projeto Movie Card Library Crud - Envio item 6

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -5,6 +5,7 @@
 // https://www.youtube.com/watch?v=h0sNAXE1ozo
 
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import MovieCard from '../components/MovieCard';
 import Loading from '../components/Loading';
 import * as movieAPI from '../services/movieAPI';
@@ -39,6 +40,7 @@ class MovieList extends Component {
     return (
       <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        <Link to="/movies/new">ADICIONAR CARTÃO</Link>
       </div>
     );
   }
