9c9a1a9aff507d2f4a22622991757aef76472d3b
teofilobrandao <teofiloafssbrandao@gmail.com>
2021-06-26 21:28:55 -0300

    Projeto Movie Card Library Crud - Envio item 3

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import propTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie: { title, storyline, id } } = this.props;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <h2>{ title }</h2>
+        <p>{ storyline }</p>
+        <Link to={ `movies/${id}` }>VER DETALHES</Link>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: propTypes.shape({
+    title: propTypes.string,
+    storyline: propTypes.string,
+    id: propTypes.number,
+  }).isRequired,
+};
+
+// MovieCard.defaultProps = {
+//   title: '',
+//   storyline: '',
+//   id: '',
+// };
 
 export default MovieCard;
