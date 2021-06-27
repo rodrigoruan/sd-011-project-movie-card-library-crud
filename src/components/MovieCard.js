5b40a1c3f1a8ea59748f5e64ec956d5b52bda597
pmaiamateus <pmaiamateus@gmail.com>
2021-06-27 17:11:28 -0300

    adiciona passo 3

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import propTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie: { title, storyline, id } } = this.props;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <h4>{title}</h4>
+        <p>{storyline}</p>
+        <Link to={ `movies/${id}` }>VER DETALHES</Link>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: propTypes.shape,
+}.isRequired;
 
 export default MovieCard;
