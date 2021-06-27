7aaad2cc83b58d71e1e6e331c044f6fe70ea75db
pmaiamateus <pmaiamateus@gmail.com>
2021-06-27 16:45:27 -0300

    adiciona passo 1

--- src/App.js
+++ src/App.js
@@ -6,13 +6,12 @@ import { MovieList, NewMovie, MovieDetails, EditMovie, NotFound } from './pages'
 function App() {
   return (
     <Router>
-      <div>Movie Card Library CRUD</div>
       <Switch>
-        <Route exact path="/" render={ () => <MovieList /> } />
-        <Route exact path="/movies/new" render={ () => <NewMovie /> } />
-        <Route exact path="/movies/:id" render={ () => <MovieDetails /> } />
-        <Route exact path="/movies/:id/edit" render={ () => <EditMovie /> } />
-        <Route NoMatch render={ () => <NotFound /> } />
+        <Route exact path="/" component={ MovieList } />
+        <Route exact path="/movies/new" component={ NewMovie } />
+        <Route exact path="/movies/:id" component={ MovieDetails } />
+        <Route exact path="/movies/:id/edit" component={ EditMovie } />
+        <Route exact path="*" component={ NotFound } />
       </Switch>
     </Router>
   );
