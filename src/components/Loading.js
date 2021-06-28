828ab85ab56824b16f04d63065593413d2caa9b0
Nick-713 <pereiranicolas713@gmail.com>
2021-06-27 23:24:41 -0300

    Carregando...

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class Loading extends Component {
   render() {
     return (
-      <div>Loading message</div>
+      <div>Carregando...</div>
     );
   }
 }
