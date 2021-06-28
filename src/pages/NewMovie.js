eabc31589ad9e1429381afc7977f923c878c7d26
Anibal <anibalneto29@gmail.com>
2021-06-28 20:39:06 -0300

    Requisito 6: Insira um link na página inicial para NewMovie para criar novos cartões

--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
-
+import { Redirect } from 'react-router-dom';
 import MovieForm from '../components/MovieForm';
-// import * as movieAPI from '../services/movieAPI';
+import * as movieAPI from '../services/movieAPI';
 
 class NewMovie extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      shouldRedirect: false,
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleSubmit(newMovie) {
+    this.setState({ shouldRedirect: true });
+    movieAPI.createMovie(newMovie);
   }
 
   render() {
+    const { shouldRedirect } = this.state;
+
+    if (shouldRedirect) {
+      return <Redirect to="/" />;
+    }
     return (
       <div data-testid="new-movie">
         <MovieForm onSubmit={ this.handleSubmit } />
@@ -21,3 +31,5 @@ class NewMovie extends Component {
   }
 }
 export default NewMovie;
+
+// Source: consulta ao repositório https://github.com/tryber/sd-011-project-movie-card-library-crud/pull/161/
