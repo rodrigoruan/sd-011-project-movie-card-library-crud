e4cc0ae967f0e86501bc3da08789f27f5888f78e
Ana Jacomassi <anajacomassip@gmail.com>
2021-06-28 19:21:18 -0300

    Enfim, requisito 4

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -5,12 +5,17 @@ import { Link } from 'react-router-dom';
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, storyline, id } = movie;
+    const { imagepath, title, subtitle, genre, storyline, id } = movie;
     return (
-      <div data-testid="movie-card">
-        <h3>{ title }</h3>
-        <p>{ storyline }</p>
-        <Link to={ `movies/${id}` }>VER DETALHES</Link>
+      <div>
+        <img alt="Movie Cover" src={ imagepath } />
+        <div data-testid="movie-card">
+          <h3>{ title }</h3>
+          <h4>{ subtitle }</h4>
+          <h5>{ genre }</h5>
+          <p>{ storyline }</p>
+          <Link to={ `movies/${id}` }>VER DETALHES</Link>
+        </div>
       </div>
     );
   }
@@ -18,7 +23,10 @@ class MovieCard extends React.Component {
 
 MovieCard.propTypes = {
   movie: PropTypes.objectOf({
+    imagepath: PropTypes.string,
     title: PropTypes.string,
+    subtitle: PropTypes.string,
+    genre: PropTypes.string,
     storyline: PropTypes.string,
     id: PropTypes.number,
   }).isRequired,
