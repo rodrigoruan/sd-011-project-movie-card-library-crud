42e2af2e47239ed60dc2762e29c2fd612dbad3e4
Anibal <anibalneto29@gmail.com>
2021-06-28 18:40:00 -0300

    [anibalfn] project-movie-card-library-crud

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie'
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={ MovieList } />
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route exact path="/movies/:id/edit" component={ EditMovie } />
+        <Route path="/movies/id" render={ (props) => <MovieDetails {...props } /> } />
+        <Route component={ NotFound } />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+// Source: consulta ao repositório https://github.com/tryber/sd-011-project-movie-card-library-crud/pull/165/
