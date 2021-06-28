db7147e63ce3b00a8af8db1d587dc5a5b9b42895
Nick-713 <pereiranicolas713@gmail.com>
2021-06-27 23:55:20 -0300

    App.js completo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import { MovieList, NewMovie, MovieDetails, EditMovie, NotFound } from './pages';
 
 export default class App extends Component {
   render() {
     return (
-      <div>
+      <BrowserRouter>
         <div>Movie Card Library CRUD</div>
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="/movies/new" component={ NewMovie } />
-            <Route exact path="/movies/:id/edit" component={ EditMovie } />
-            <Route exact path="/movies/:id" component={ MovieDetails } />
-            <Route exact path="/" component={ MovieList } />
-            <Route component={ NotFound } />
-          </Switch>
-        </BrowserRouter>
-      </div>
+        <Link to="/movie/new">ADICIONAR CARTÃO</Link>
+        <Switch>
+          <Route exact path="/movies/new" component={ NewMovie } />
+          <Route exact path="/movies/:id/edit" component={ EditMovie } />
+          <Route exact path="/movies/:id" component={ MovieDetails } />
+          <Route exact path="/" component={ MovieList } />
+          <Route component={ NotFound } />
+        </Switch>
+      </BrowserRouter>
+
     );
   }
 }
