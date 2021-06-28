3ab94f7ac8bdf486bb6bc3871af35585530a7739
Nick-713 <pereiranicolas713@gmail.com>
2021-06-27 22:40:20 -0300

    req 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './App.css';
 
-function App() {
-  return (
-    <div>Movie Card Library CRUD</div>
-  );
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { MovieList, NewMovie, MovieDetails, EditMovie, NotFound } from './pages';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <div>Movie Card Library CRUD</div>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/movies/new" component={ NewMovie } />
+            <Route exact path="/movies/:id/edit" component={ EditMovie } />
+            <Route exact path="/movies/:id" component={ MovieDetails } />
+            <Route exact path="/" component={ MovieList } />
+            <Route component={ NotFound } />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
-
-export default App;
