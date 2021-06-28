b8b5dd3d0cf0111abd49f5074eb2444069bf1691
Nikolas <nikolas.silva1990@gmail.com>
2021-06-28 12:11:02 -0300

    Requisito 6 concluído. Conflitos de Lint resolvidos.

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import MovieCard from '../components/MovieCard';
 import * as movieAPI from '../services/movieAPI';
 import Loading from '../components/Loading';
@@ -32,6 +32,7 @@ class MovieList extends Component {
       <div data-testid="movie-list">
         {loading ? <Loading />
           : movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        <Link to="/movies/new">ADICIONAR CARTÃO</Link>
       </div>);
   }
 }
