653bfb7dad22f085938c21a8a3395c6f5b43af72
Nick-713 <pereiranicolas713@gmail.com>
2021-06-28 00:33:55 -0300

    requisito 1 -completo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
-import './App.css';
 
 import MovieList from './pages/MovieList';
 import NewMovie from './pages/NewMovie';
