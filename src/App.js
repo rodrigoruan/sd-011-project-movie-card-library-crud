0057bfe27b822dd92ee47bdaa97571f5b48896ef
Ana Jacomassi <anajacomassip@gmail.com>
2021-06-28 17:41:19 -0300

    Adicina requistos 1 a 3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <Router>
-      <div>Movie Card Library CRUD</div>
-    </Router>
-  );
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
+
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>Movie Card Library CRUD</div>
+        <Switch>
+          <Route exact path="/movies/new" component={ NewMovie } />
+          <Route
+            exact
+            path="/movies/:id"
+            render={ (props) => <MovieDetails { ...props } /> }
+          />
+          <Route exact path="/movies/:id/edit" component={ EditMovie } />
+          <Route exact path="/" component={ MovieList } />
+          <Route path="*" component={ NotFound } />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
