aada8ac91494f6d7e697c323b699172e26fb1647
Nick-713 <pereiranicolas713@gmail.com>
2021-06-27 22:45:50 -0300

    proptype MovieForm

--- src/components/MovieForm.js
+++ src/components/MovieForm.js
@@ -164,4 +164,9 @@ class MovieForm extends React.Component {
   }
 }
 
+MovieForm.propTypes = {
+  movie: PropTypes.shape(PropTypes.object).isRequired,
+  onSubmit: PropTypes.func.isRequired,
+};
+
 export default MovieForm;
