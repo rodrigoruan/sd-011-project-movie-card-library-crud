390461d507080783b06396f58da823b2d3bc8da0
siebenei5 <siebenei7@gmail.com>
2021-06-29 02:58:38 +0000

    delete component in moviedetails render

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -52,6 +52,7 @@ class MovieDetails extends Component {
           <p>{ `Rating: ${rating}` }</p>
           <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
           <Link to="/">VOLTAR</Link>
+          <Link to="/" onClick={ this.deleteMovie }>DELETAR</Link>
         </div>
       )
     );
