13ea5918315120562cf930026df3ae545d909971
Jander Liborio <jandernunes14@gmail.com>
2021-06-29 00:59:12 -0400

    esquecia de importar os caminhos

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import NewMovie from './pages/NewMovie';
+import MovieDetails from './pages/MovieDetails';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 import './App.css';
 
 // ajuda do italo de matos turma 10-b
