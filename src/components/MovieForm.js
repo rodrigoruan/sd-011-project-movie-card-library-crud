657b1b147c1e0956e529ab5e805bcef671539ddc
Lais Simenikim <l.simenikim@gmail.com>
2021-06-28 22:43:28 -0300

    ajusta lint

--- src/components/MovieForm.js
+++ src/components/MovieForm.js
@@ -164,4 +164,22 @@ class MovieForm extends React.Component {
   }
 }
 
+MovieForm.propTypes = {
+  movie: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+    imagePath: PropTypes.string.isRequired,
+    bookmarked: PropTypes.bool.isRequired,
+    genre: PropTypes.string.isRequired,
+  }),
+  onSubmit: PropTypes.func.isRequired,
+};
+
+MovieForm.defaultProps = {
+  movie: {},
+};
+
 export default MovieForm;
