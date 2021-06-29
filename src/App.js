f1bc712fea981066fbed24c05e1edcb0283a78e9
Marcos Mantovani <mvmes23@gmail.com>
2021-06-29 10:31:25 -0300

    last Fix, agora vai3, -.- LINTtttttt

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
-import './App.css';
 import MovieList from './pages/MovieList';
 import MovieDetails from './pages/MovieDetails';
 import NewMovie from './pages/NewMovie';
