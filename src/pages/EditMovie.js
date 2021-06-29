8be150e164e816444295a1d51b22cf616ae8e6fa
Ana Jacomassi <anajacomassip@gmail.com>
2021-06-28 22:45:03 -0300

    Requisitos 5 e 6

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,26 +1,48 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router';
+import PropTypes from 'prop-types';
 
 import { MovieForm } from '../components';
 import * as movieAPI from '../services/movieAPI';
+import Loading from '../components/Loading';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      movie: [],
+      status: 'loading',
+      shouldRedirect: false,
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  componentDidMount() {
+    const { match } = this.props;
+    const { id } = match.params;
+
+    movieAPI.getMovie(id)
+      .then((data) => {
+        this.setState({ movie: data, status: false });
+      })
+      .catch((error) => console.log(error));
+  }
+
   handleSubmit(updatedMovie) {
+    this.setState({
+      shouldRedirect: true,
+    });
+    movieAPI.updateMovie(updatedMovie);
   }
 
   render() {
     const { status, shouldRedirect, movie } = this.state;
     if (shouldRedirect) {
-      // Redirect
+      return <Redirect to="/" />;
     }
 
     if (status === 'loading') {
-      // render Loading
+      return <Loading />;
     }
 
     return (
@@ -30,5 +52,9 @@ class EditMovie extends Component {
     );
   }
 }
+
+EditMovie.propTypes = {
+  match: PropTypes.arrayOf,
+}.isRequired;
 
 export default EditMovie;
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
 import MovieCard from '../components/MovieCard';
 import Loading from '../components/Loading';
-
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
@@ -27,8 +28,11 @@ class MovieList extends Component {
     // Render Loading here if the request is still happening
     if (loading) return <Loading />;
     return (
-      <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+      <div>
+        <div data-testid="movie-list">
+          {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        </div>
+        <Link to="/movies/new">ADICIONAR CARTÃO</Link>
       </div>
     );
   }
