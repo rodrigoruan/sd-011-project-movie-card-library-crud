49ebf185c69da5184336d62cdea613ebad604dda
JoaoCabrera <joaocabrerati@gmail.com>
2021-06-29 00:02:58 -0300

    Adiciona requisito 5

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import './App.css';
 import MovieList from './pages/MovieList';
 import NewMovie from './pages/NewMovie';
@@ -11,6 +11,9 @@ class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
+        <Link to="/movies/new">
+          ADICIONAR CARTÃO
+        </Link>
         <Route exact path="/" component={ MovieList } />
         <Route exact path="/movies/new" component={ NewMovie } />
         <Route exact path="/movies/:id" component={ MovieDetails } />
